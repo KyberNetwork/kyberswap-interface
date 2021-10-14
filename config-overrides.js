46bfe34572bf9e8baa65f317c7233a89cfabac05
viet-nv <vietnv1304@gmail.com>
2021-10-14 14:11:02 +0700

    remove unused packages

--- config-overrides.js
+++ config-overrides.js
@@ -5,13 +5,28 @@ module.exports = function override(config, env) {
   config = rewireStyledComponents(config, env)
 
   config.optimization = {
+    ...config.optimization,
+    moduleIds: 'named',
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/](ethers|@ethersproject)[\\/]/,
           name: 'ethers',
           chunks: 'all'
-        }
+        },
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          // cacheGroupKey here is `commons` as the key of the cacheGroup
+          name(module, chunks, cacheGroupKey) {
+            const moduleFileName = module
+              .identifier()
+              .split('/')
+              .reduceRight((item) => item);
+            const allChunksNames = chunks.map((item) => item.name).join('~');
+            return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+          },
+          chunks: 'all',
+        },
       }
     }
   }
