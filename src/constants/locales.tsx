35b8c2f8e69ccd51bee78d09cb11c82661c544c3
Nam Nguyen <nam.nguyen@kyber.network>
2022-01-13 10:55:18 +0700

    Fix align flag

--- src/constants/locales.tsx
+++ src/constants/locales.tsx
@@ -13,27 +13,27 @@ export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
 export const LOCALE_LABEL: { [locale in SupportedLocale]: JSX.Element } = {
   'en-US': (
     <>
-      <img src={FlagEN} style={{ verticalAlign: 'bottom' }} /> &nbsp;English
+      <img src={FlagEN} style={{ verticalAlign: 'middle' }} /> &nbsp;English
     </>
   ),
   'zh-CN': (
     <>
-      <img src={FlagZH} style={{ verticalAlign: 'bottom' }} /> &nbsp;中文
+      <img src={FlagZH} style={{ verticalAlign: 'middle' }} /> &nbsp;中文
     </>
   ),
   'tr-TR': (
     <>
-      <img src={FlagTR} style={{ verticalAlign: 'bottom' }} /> &nbsp;Türkçe
+      <img src={FlagTR} style={{ verticalAlign: 'middle' }} /> &nbsp;Türkçe
     </>
   ),
   'ko-KR': (
     <>
-      <img src={FlagKO} style={{ verticalAlign: 'bottom' }} /> &nbsp;한국어
+      <img src={FlagKO} style={{ verticalAlign: 'middle' }} /> &nbsp;한국어
     </>
   ),
   'vi-VN': (
     <>
-      <img src={FlagVI} style={{ verticalAlign: 'bottom' }} /> &nbsp;Tiếng Việt
+      <img src={FlagVI} style={{ verticalAlign: 'middle' }} /> &nbsp;Tiếng Việt
     </>
   )
 }
