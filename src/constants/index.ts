b61e448e2d7d537b907321ae94c097d0daef1f66
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2022-02-09 13:47:29 +0700

    feat(farms): add temporary bsc farm for testing in bscmainnet

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -937,7 +937,7 @@ export const FAIRLAUNCH_V2_ADDRESSES: { [chainId in ChainId]: string[] } = {
   [ChainId.MATIC]: [],
   [ChainId.MUMBAI]: [],
   [ChainId.BSCTESTNET]: [],
-  [ChainId.BSCMAINNET]: [],
+  [ChainId.BSCMAINNET]: ['0xe444f3b2eA3F9FE1493F8a0F723023039fAFb0BA', '0xf921E60bFA20fecCfCc24d5a7cDc9bf4E9A39C7c'],
   [ChainId.AVAXTESTNET]: [],
   [ChainId.AVAXMAINNET]: [],
   [ChainId.FANTOM]: [],
