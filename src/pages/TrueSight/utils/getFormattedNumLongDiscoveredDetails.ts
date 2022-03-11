7ac2e95ac8e4bfbe169fb255140d979ccb97479c
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2022-03-11 11:00:32 +0700

    temp

--- src/pages/TrueSight/utils/getFormattedNumLongDiscoveredDetails.ts
+++ src/pages/TrueSight/utils/getFormattedNumLongDiscoveredDetails.ts
@@ -28,16 +28,16 @@ export default function getFormattedNumLongDiscoveredDetails(
     pricePercent:
       price <= 0 || price_discovered <= 0
         ? '--'
-        : formattedNumLong((price_discovered / price) * 100 - 100, false) + '%',
+        : formattedNumLong((price / price_discovered) * 100 - 100, false) + '%',
     tradingVolume: trading_volume_discovered <= 0 ? '--' : formattedNumLong(trading_volume_discovered, true),
     tradingVolumePercent:
       trading_volume <= 0 || trading_volume_discovered <= 0
         ? '--'
-        : formattedNumLong((trading_volume_discovered / trading_volume) * 100 - 100, false) + '%',
+        : formattedNumLong((trading_volume / trading_volume_discovered) * 100 - 100, false) + '%',
     marketCap: market_cap_discovered <= 0 ? '--' : formattedNumLong(market_cap_discovered, true),
     marketCapPercent:
       market_cap <= 0 || market_cap_discovered <= 0
         ? '--'
-        : formattedNumLong((market_cap_discovered / market_cap) * 100 - 100, false) + '%',
+        : formattedNumLong((market_cap / market_cap_discovered) * 100 - 100, false) + '%',
   }
 }
