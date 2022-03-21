e9dacdbce254bad382772350ee9cc629f6cea199
tien7668 <tien7668@gmail.com>
2022-03-21 22:57:14 +0700

    add test token, update contract and init hash

--- src/constants/tokenLists/rinkeby.tokenlist.ts
+++ src/constants/tokenLists/rinkeby.tokenlist.ts
@@ -17,6 +17,20 @@ export const RINKEBY_TOKEN_LIST = {
       symbol: 'UNI',
       name: 'UNI',
       decimals: 18
+    },
+    {
+      chainId: 4,
+      address: '0x6f072Ce36c65Fd5A1f2534E5598E43b9d2fa7c77',
+      symbol: 'T1',
+      name: 'TestToken1',
+      decimals: 18
+    },
+    {
+      chainId: 4,
+      address: '0x07EfDB214ee4260F3351e1048766fcd225E6E25e',
+      symbol: 'T2',
+      name: 'TestToken2',
+      decimals: 18
     }
   ],
   version: {
