abbae72195e3744272c9a781adae41972ee38055
Đinh Văn tiến <tiendv.52@gmail.com>
2022-04-26 13:58:46 +0700

    Minor

--- src/hooks/useSwapV2Callback.ts
+++ src/hooks/useSwapV2Callback.ts
@@ -567,7 +567,7 @@ export function useSwapV2Callback(
         from: account,
         to: trade.routerAddress,
         data: trade.encodedSwapData,
-        value: JSBI.toNumber(trade.inputAmount.raw)
+        value: '0x' + trade.inputAmount.raw.toString(16)
       }
 
       const gasEstimate = await library
@@ -589,7 +589,7 @@ export function useSwapV2Callback(
         data: trade.encodedSwapData,
         gasLimit: calculateGasMargin(gasEstimate),
         ...(gasPrice?.standard ? { gasPrice: ethers.utils.parseUnits(gasPrice?.standard, 'wei') } : {}),
-        ...(trade.inputAmount.currency instanceof Token ? {} : { value: JSBI.toNumber(trade.inputAmount.raw) }),
+        ...(trade.inputAmount.currency instanceof Token ? {} : { value: '0x' + trade.inputAmount.raw.toString(16) }),
       }
 
       return library
