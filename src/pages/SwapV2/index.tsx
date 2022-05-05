98110be1007dc046e55f948049ea127942bd1e41
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2022-05-05 16:55:27 +0700

    Change network correctly in top trending tokens widget

--- src/pages/SwapV2/index.tsx
+++ src/pages/SwapV2/index.tsx
@@ -182,7 +182,6 @@ export default function Swap({ history }: RouteComponentProps) {
   // reset if they close warning without tokens in params
   const handleDismissTokenWarning = useCallback(() => {
     setDismissTokenWarning(true)
-    history.push('/swap')
   }, [history])
 
   // modal and loading
