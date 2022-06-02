f20d8c83e92493b08f411f84182d85bb17a7b40e
Tien Phan <tienpq149@gmail.com>
2022-06-02 17:04:23 +0700

    chore: cleaning up

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -18,18 +18,15 @@ export const fetchToken = () => {
   })
     .then(currentToken => {
       if (currentToken) {
-        console.log('current token for client: ', currentToken)
         return currentToken
         // Track the token -> client mapping, by sending to backend server
         // show on the UI that permission is secured
       } else {
-        console.log('No registration token available. Request permission to generate one.')
         return ''
         // shows on the UI that permission is required
       }
     })
     .catch(err => {
-      console.log('An error occurred while retrieving token. ', err)
       // catch error while creating client token
       return ''
     })
