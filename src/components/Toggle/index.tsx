f9343a203bfdc9d55fd128799dad54b121795fe0
Doan Sy Hung <hungdoansy@gmail.com>
2022-07-12 15:27:10 +0700

    wip(toggle): move Dot out of Toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -10,10 +10,25 @@ export interface ToggleProps {
   style?: CSSProperties
 }
 
+export const Dot = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 4px;
+
+  width: 20px;
+  height: 20px;
+
+  background: ${({ theme }) => theme.border};
+  border-radius: 50%;
+
+  transform: translateY(-50%);
+  transition: all 0.2s ease-in-out;
+`
+
 const Toggle: React.FC<ToggleProps> = ({ id, isActive, toggle, style, className }) => {
   return (
-    <div id={id} onClick={toggle} style={style} data-active={isActive} className={`toggle ${className}`}>
-      <div className="dot" />
+    <div id={id} onClick={toggle} style={style} data-active={isActive} className={className}>
+      <Dot />
     </div>
   )
 }
@@ -27,25 +42,10 @@ export default styled(Toggle)`
   transition: all 0.2s ease-in-out;
   cursor: pointer;
 
-  .dot {
-    position: absolute;
-    top: 50%;
-    left: 4px;
-
-    width: 20px;
-    height: 20px;
-
-    background: ${({ theme }) => theme.border};
-    border-radius: 50%;
-
-    transform: translateY(-50%);
-    transition: all 0.2s ease-in-out;
-  }
-
   &[data-active='true'] {
     background: ${({ theme }) => rgba(theme.primary, 0.2)};
 
-    .dot {
+    ${Dot} {
       background: ${({ theme }) => theme.primary};
       left: 32px;
     }
