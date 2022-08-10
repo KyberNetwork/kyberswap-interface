d6fc59a23cef2a3037c1d433af8142d21ca4f4ce
Doan Sy Hung <hungdoansy@gmail.com>
2022-08-10 09:41:04 +0700

    simplify checkForBraveBrowser

--- src/utils/checkForBraveBrowser.ts
+++ src/utils/checkForBraveBrowser.ts
@@ -1,11 +1,7 @@
 // https://stackoverflow.com/questions/36523448/how-do-i-tell-if-a-user-is-using-brave-as-their-browser
 let isBraveBrowser = false
-;(() => {
-  const check = async () => {
-    const result = (navigator.brave && (await navigator.brave.isBrave())) || false
-    isBraveBrowser = result
-  }
-  check()
+;(async () => {
+  isBraveBrowser = (navigator.brave && (await navigator.brave.isBrave())) || false
 })()
 
 const checkForBraveBrowser = () => {
