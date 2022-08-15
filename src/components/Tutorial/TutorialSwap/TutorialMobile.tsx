84967b323c6502c08b3ad50afd57b50686486759
Danh <nguyenhoaidanh2096@gmail.com>
2022-08-15 14:16:14 +0700

    fix: modal tut mobile

--- src/components/Tutorial/TutorialSwap/TutorialMobile.tsx
+++ src/components/Tutorial/TutorialSwap/TutorialMobile.tsx
@@ -7,6 +7,8 @@ import ToggleCollapse, { ToggleItemType } from 'components/Collapse'
 import { MobileModalWrapper } from 'components/swapv2/styleds'
 import useTheme from 'hooks/useTheme'
 
+import { TOTAL_STEP } from './constant'
+
 export default function TutorialMobile({
   stopTutorial,
   steps,
@@ -18,22 +20,33 @@ export default function TutorialMobile({
 }) {
   const theme = useTheme()
   return (
-    <MobileModalWrapper isOpen={isOpen} onDismiss={stopTutorial} maxHeight="100vh">
+    <MobileModalWrapper
+      isOpen={isOpen}
+      onDismiss={stopTutorial}
+      maxHeight={`${window.innerHeight}px`}
+      borderRadius={'0px'}
+    >
       <Flex
         flexDirection="column"
         alignItems="center"
         width="100%"
         style={{
           background: theme.buttonGray,
+          overflow: 'auto',
         }}
       >
-        <Flex width="100%" padding="16px 16px 0px" marginBottom="1rem" justifyContent="space-between">
-          <Text fontSize={16} fontWeight={500} color={theme.text}>
-            <Trans>Welcome to KyberSwap!</Trans>
+        <div style={{ marginBottom: '1rem', padding: '16px 16px 0px', width: '100%' }}>
+          <Flex justifyContent="space-between" marginBottom="10px">
+            <Text fontSize={16} fontWeight={500} color={theme.text}>
+              <Trans>Welcome to KyberSwap!</Trans>
+            </Text>
+            <X color={theme.subText} size={24} onClick={stopTutorial} />
+          </Flex>
+          <Text fontSize={12} color={theme.subText}>
+            {TOTAL_STEP} <Trans>easy ways to get started with KyberSwap</Trans>
           </Text>
-          <X color={theme.subText} size={24} onClick={stopTutorial} />
-        </Flex>
-        <div style={{ height: '100vh', width: '100%', overflow: 'auto' }}>
+        </div>
+        <div style={{ width: '100%' }}>
           <ToggleCollapse
             data={steps}
             itemStyle={{ background: theme.buttonGray }}
