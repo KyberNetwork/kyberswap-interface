ad5b2b70054f340771bde5996b50437c6c47e138
Diep Pham <xiaoyhun@gmail.com>
2022-09-22 09:39:48 +0700

    fix: add back money bag icon for staked position

--- src/pages/ProAmmPool/index.tsx
+++ src/pages/ProAmmPool/index.tsx
@@ -223,7 +223,12 @@ export default function ProAmmPool() {
               {/* Use display attribute here instead of condition rendering to prevent re-render full list when toggle showStaked => increase performance */}
               <PositionCardGrid style={{ display: showStaked ? 'none' : 'grid' }}>
                 {filteredPositions.map(p => (
-                  <PositionListItem refe={tokenAddressSymbolMap} positionDetails={p} key={p.tokenId.toString()} />
+                  <PositionListItem
+                    refe={tokenAddressSymbolMap}
+                    positionDetails={p}
+                    key={p.tokenId.toString()}
+                    farmAvailable={!!p.stakedLiquidity}
+                  />
                 ))}
               </PositionCardGrid>
               <PositionCardGrid style={{ display: !showStaked ? 'none' : 'grid' }}>
