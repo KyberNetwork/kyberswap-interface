7b16ebf6f5ed61ce8a5d5c34824febdb94bf8c37
Doan Sy Hung <hungdoansy@gmail.com>
2022-10-04 14:58:08 +0700

    wip: import bridge - this commit should be moved to last

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -2,6 +2,7 @@ import { RouteComponentProps } from 'react-router-dom'
 
 import { Container, PageWrapper } from 'components/swapv2/styleds'
 
+import BridgeHistory from './BridgeHistory'
 import SwapForm from './SwapForm'
 
 export default function Bridge({ history }: RouteComponentProps) {
@@ -9,6 +10,7 @@ export default function Bridge({ history }: RouteComponentProps) {
     <PageWrapper>
       <Container>
         <SwapForm />
+        <BridgeHistory />
       </Container>
     </PageWrapper>
   )
