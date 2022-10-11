20a9c4965b6e2e6f0d5594400a38c1fcb0889768
Diep Pham <xiaoyhun@gmail.com>
2022-10-11 15:56:00 +0700

    chore: remove redundant code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -394,10 +394,7 @@ export const detectInjectedType = (): 'COIN98' | 'BRAVE' | 'METAMASK' | 'COINBAS
   const isMetamask = !!ethereum?.isMetaMask
   const isCoin98 = isMetamask && ethereum.isCoin98
   const isBraveBrowser = checkForBraveBrowser()
-  const isCoinbase =
-    window.ethereum?.isCoinbaseWallet ||
-    window.ethereum?.selectedProvider?.isCoinbaseWallet ||
-    window.ethereum?.providers
+  const isCoinbase = ethereum?.isCoinbaseWallet
 
   if (isCoinbase) return 'COINBASE'
   if (isCoin98) return 'COIN98'
