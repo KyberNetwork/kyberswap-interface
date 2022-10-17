8d9cc95e52eb86ab7b5aacc1c75641f872a5e653
Nam Nguyen <nam.nguyen@kyber.network>
2022-10-17 10:41:14 +0700

    fix revert
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -144,6 +144,8 @@ const ClaimRewardButton = styled(ButtonPrimary)`
 export const NewLabel = styled.span`
   font-size: 10px;
   color: ${({ theme }) => theme.red};
+  height: calc(100% + 4px);
+  margin-left: 2px;
 `
 
 export default function Menu() {
