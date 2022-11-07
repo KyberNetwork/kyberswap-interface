b2ff5ab27982e460c35e25edb38ffb3472c8f4e9
Doan Sy Hung <hungdoansy@gmail.com>
2022-11-07 10:47:40 +0700

    feat: implement ChangeLog button

---
+++ src/components/ChangeLog/index.tsx
@@ -0,0 +1,89 @@
+import { useRef, useState } from 'react'
+import { Box } from 'rebass'
+import styled, { css } from 'styled-components'
+
+import { ReactComponent as NotificationIcon2 } from 'assets/svg/slim_notification_icon.svg'
+import { useOnClickOutside } from 'hooks/useOnClickOutside'
+
+const StyledMenuIcon = styled(NotificationIcon2)`
+  height: 22px;
+  width: 22px;
+
+  > * {
+    stroke: ${({ theme }) => theme.text};
+  }
+`
+
+const StyledButton = styled.button<{ active?: boolean }>`
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: transparent;
+  margin: 0;
+  padding: 0;
+  height: 40px;
+  width: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.text};
+
+  border-radius: 999px;
+
+  :hover {
+    cursor: pointer;
+    outline: none;
+    background-color: ${({ theme }) => theme.buttonBlack};
+  }
+
+  ${({ active }) =>
+    active
+      ? css`
+          cursor: pointer;
+          outline: none;
+          background-color: ${({ theme }) => theme.buttonBlack};
+        `
+      : ''}
+`
+const StyledIframe = styled.iframe`
+  position: absolute;
+  top: calc(100% + 16px);
+  right: 0;
+  border: 1px solid ${({ theme }) => theme.border};
+  border-radius: 16px;
+  overflow: hidden;
+`
+
+const ChangeLog = () => {
+  const [isOpen, setOpen] = useState(false)
+  const ref = useRef<HTMLButtonElement>(null)
+
+  useOnClickOutside(ref, () => {
+    setOpen(false)
+  })
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+      }}
+      ref={ref}
+    >
+      <StyledButton onClick={() => setOpen(o => !o)}>
+        <StyledMenuIcon data-badge-changelog />
+      </StyledButton>
+
+      <StyledIframe
+        title="changelog"
+        style={{
+          display: isOpen ? 'block' : 'none',
+        }}
+        width="400"
+        height="600"
+        src="https://embed-316782075.sleekplan.app/#/changelog"
+      />
+    </Box>
+  )
+}
+
+export default ChangeLog
