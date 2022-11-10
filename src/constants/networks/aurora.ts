6b54b8767735c9fb23d83184e19da7bc0f403a7b
Doan Sy Hung <hungdoansy@gmail.com>
2022-11-10 10:40:57 +0700

    chore: remove typo

--- src/constants/networks/aurora.ts
+++ src/constants/networks/aurora.ts
@@ -53,7 +53,7 @@ const auroraInfo: NetworkInfo = {
     nonfungiblePositionManager: '0x2B1c7b41f6A8F2b2bc45C3233a5d5FB3cD6dC9A8',
     tickReader: '0x165c68077ac06c83800d19200e6E2B08D02dE75D',
     initCodeHash: '0xc597aba1bb02db42ba24a8878837965718c032f8b46be94a6e46452a9f89ca01',
-    quoter: '0x0D125c15D54cA1F8aw813C74A81aEe34ebB508C1f',
+    quoter: '0x0D125c15D54cA1F8a813C74A81aEe34ebB508C1f',
     routers: '0xC1e7dFE73E1598E3910EF4C7845B68A9Ab6F4c83',
   },
   averageBlockTimeInSeconds: 1,
