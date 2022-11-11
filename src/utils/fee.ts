2a5c20409f349f91a1912259b34dcfe66228a97a
Nam Nguyen <nam.nguyen@kyber.network>
2022-11-11 14:53:32 +0700

    fix: crash when formatting very small number

--- src/utils/fee.ts
+++ src/utils/fee.ts
@@ -7,6 +7,24 @@ import { FeeConfig } from 'hooks/useSwapV2Callback'
 import { Aggregator } from 'utils/aggregator'
 import { formattedNum } from 'utils/index'
 
+function toFixed(x: number): string {
+  if (Math.abs(x) < 1.0) {
+    const e = parseInt(x.toString().split('e-')[1])
+    if (e) {
+      x *= Math.pow(10, e - 1)
+      return '0.' + '0'.repeat(e - 1) + x.toString().substring(2)
+    }
+  } else {
+    let e = parseInt(x.toString().split('+')[1])
+    if (e > 20) {
+      e -= 20
+      x /= Math.pow(10, e)
+      return x.toString() + '0'.repeat(e)
+    }
+  }
+  return x.toString()
+}
+
 /**
  * Get Fee Amount in a Trade (unit: USD)
  * @param trade
@@ -15,16 +33,16 @@ import { formattedNum } from 'utils/index'
 export function getFormattedFeeAmountUsd(trade: Aggregator, feeConfig: FeeConfig | undefined) {
   if (feeConfig) {
     const amountInUsd = new Fraction(
-      parseUnits(trade.amountInUsd.toString(), RESERVE_USD_DECIMALS).toString(),
+      parseUnits(toFixed(trade.amountInUsd), RESERVE_USD_DECIMALS).toString(),
       JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(RESERVE_USD_DECIMALS)),
     )
-    // feeAmount might < 1.
-    const feeAmountFraction = new Fraction(
-      parseUnits(feeConfig.feeAmount, RESERVE_USD_DECIMALS).toString(),
-      JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(RESERVE_USD_DECIMALS)),
-    )
-    const feeAmountDecimal = feeAmountFraction.divide(BIPS_BASE)
     if (amountInUsd) {
+      // feeAmount might < 1.
+      const feeAmountFraction = new Fraction(
+        parseUnits(feeConfig.feeAmount, RESERVE_USD_DECIMALS).toString(),
+        JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(RESERVE_USD_DECIMALS)),
+      )
+      const feeAmountDecimal = feeAmountFraction.divide(BIPS_BASE)
       const feeAmountUsd = amountInUsd.multiply(feeAmountDecimal).toSignificant(RESERVE_USD_DECIMALS)
       return formattedNum(feeAmountUsd, true)
     }
