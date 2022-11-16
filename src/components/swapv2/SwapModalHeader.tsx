6721642d5396de7886d63afeffcf4aaa6b8361eb
Nam Nguyen <nam.nguyen@kyber.network>
2022-11-17 02:23:43 +0700

    fix: duplicate text

--- src/components/swapv2/SwapModalHeader.tsx
+++ src/components/swapv2/SwapModalHeader.tsx
@@ -118,18 +118,17 @@ export default function SwapModalHeader({
               {slippageAdjustedAmounts[Field.INPUT]?.toSignificant(6)} {nativeInput?.symbol}
             </b>
             {t` or the transaction will revert.`}
-            {t` or the transaction will revert.`}
           </TYPE.italic>
         )}
       </AutoColumn>
-      {recipient !== null ? (
+      {recipient !== null && (
         <AutoColumn justify="flex-start" gap="sm" style={{ padding: '12px 0 0 0px' }}>
           <TYPE.main>
             Output will be sent to{' '}
             <b title={recipient}>{isAddress(chainId, recipient) ? shortenAddress(chainId, recipient) : recipient}</b>
           </TYPE.main>
         </AutoColumn>
-      ) : null}
+      )}
     </AutoColumn>
   )
 }
