e21af5ee41edc6155ed4b8b81a8c01c640cf9e2f
Nam Nguyen <nam.nguyen@kyber.network>
2022-12-01 11:54:49 +0700

    fix: loading icon

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -33,8 +33,16 @@ export default function Loader({
   strokeWidth?: string
   [k: string]: any
 }) {
+  const sWN = Number(strokeWidth)
   return (
-    <StyledSVG viewBox="1 1 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" size={size} stroke={stroke} {...rest}>
+    <StyledSVG
+      viewBox={`${2.5 - sWN} ${2.5 - sWN} ${19 + sWN * 2} ${19 + sWN * 2}`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      size={size}
+      stroke={stroke}
+      {...rest}
+    >
       <path
         d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 9.27455 20.9097 6.80375 19.1414 5"
         strokeWidth={strokeWidth}
