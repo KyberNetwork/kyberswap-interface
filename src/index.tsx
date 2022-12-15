6bf48360832cdf29f1a9755f9e698f8b5f3f2461
Danh <nguyenhoaidanh2096@gmail.com>
2022-12-15 13:19:53 +0700

    enable mobile

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ import 'swiper/swiper.min.css'
 import SolanaWalletContext from 'components/SolanaWalletContext'
 import { ENV_LEVEL, ENV_TYPE, GTM_ID, MIXPANEL_PROJECT_TOKEN, SENTRY_DNS, TAG } from 'constants/env'
 import * as ENV from 'constants/env'
+import { Z_INDEXS } from 'constants/styles'
 // import { updateServiceWorker } from 'state/application/actions'
 import CampaignsUpdater from 'state/campaigns/updater'
 import { MEDIA_WIDTHS } from 'theme'
@@ -85,17 +86,19 @@ if (ENV_LEVEL > ENV_TYPE.LOCAL) {
 
 AOS.init()
 
-if (window.innerWidth > MEDIA_WIDTHS.upToSmall) {
-  const script = document.createElement('script')
-  script.src = 'https://static.zdassets.com/ekr/snippet.js?key=a73faacd-ba50-493a-8bf5-4b6878035346'
-  script.id = 'ze-snippet'
-  document.body.appendChild(script)
-  window.zESettings = {
-    webWidget: {
-      offset: { vertical: window.innerWidth > MEDIA_WIDTHS.upToLarge ? '0px' : '75px' },
-      zIndex: 1,
+// customer support widget
+const script = document.createElement('script')
+script.src = 'https://static.zdassets.com/ekr/snippet.js?key=a73faacd-ba50-493a-8bf5-4b6878035346'
+script.id = 'ze-snippet'
+document.body.appendChild(script)
+window.zESettings.offset = {
+  offset: {
+    vertical: window.innerWidth > MEDIA_WIDTHS.upToLarge ? '0px' : '75px',
+    mobile: {
+      vertical: '75px',
     },
-  }
+  },
+  zIndex: Z_INDEXS.ICON_SUPPORT,
 }
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
