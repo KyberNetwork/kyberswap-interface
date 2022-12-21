f7d8609a32dcd9860059cef46d178e4ce041a656
Doan Sy Hung <hungdoansy@gmail.com>
2022-12-21 13:29:41 +0700

    refactor: config for swap v3 route

--- src/pages/SwapV3/redirects.tsx
+++ src/pages/SwapV3/redirects.tsx
@@ -1,20 +1,22 @@
 import { Navigate, useLocation } from 'react-router-dom'
 
-import { APP_PATHS, SUPPORT_LIMIT_ORDER } from 'constants/index'
+import { APP_PATHS } from 'constants/index'
 import { useActiveWeb3React } from 'hooks'
 
-// Redirects to swap but only replace the pathname
-export function RedirectPathToSwapNetwork() {
-  const location = useLocation
+// Redirects to swap-v3 but only replace the pathname
+export function RedirectPathToSwapV3Network() {
   const { networkInfo } = useActiveWeb3React()
-  const { pathname } = useLocation()
+  const location = useLocation()
+  const { pathname } = location
+
+  let redirectTo = [APP_PATHS.LIMIT, APP_PATHS.SWAP_V3, APP_PATHS.SWAP].find(path => pathname.startsWith(path))
+  redirectTo ||= APP_PATHS.SWAP
+
   return (
     <Navigate
       to={{
         ...location,
-        pathname:
-          `${pathname.startsWith(APP_PATHS.LIMIT) && SUPPORT_LIMIT_ORDER ? APP_PATHS.LIMIT : APP_PATHS.SWAP}/` +
-          networkInfo.route,
+        pathname: `${redirectTo}/` + networkInfo.route,
       }}
     />
   )
