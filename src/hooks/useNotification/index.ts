fb0f7c7edc7233e32bfe2929e5bf1c88077201dc
Danh <nguyenhoaidanh2096@gmail.com>
2022-12-29 13:04:09 +0700

    refactor

--- src/hooks/useNotification/index.ts
+++ src/hooks/useNotification/index.ts
@@ -8,6 +8,7 @@ import { KS_SETTING_API, NOTIFICATION_API } from 'constants/env'
 import { useActiveWeb3React } from 'hooks'
 import { AppState } from 'state'
 import { setLoadingNotification, setSubscribedNotificationTopic } from 'state/application/actions'
+import { useNotificationModalToggle } from 'state/application/hooks'
 
 const getAllTopicUrl = (account: string | null | undefined) =>
   `${KS_SETTING_API}/v1/notification/topic-groups${account ? `?walletAddress=${account}` : ''}`
@@ -30,6 +31,7 @@ const useNotification = () => {
   const { isLoading, topicGroups, userInfo } = useSelector((state: AppState) => state.application.notification)
 
   const { account, chainId } = useActiveWeb3React()
+  const toggleSubscribeModal = useNotificationModalToggle()
   const dispatch = useDispatch()
 
   const setLoading = useCallback(
@@ -38,7 +40,6 @@ const useNotification = () => {
     },
     [dispatch],
   )
-
   const { data: resp } = useSWR(
     getAllTopicUrl(account),
     (url: string) => {
@@ -111,17 +112,21 @@ const useNotification = () => {
       } finally {
         setLoading(false)
       }
-      return
     },
     [setLoading, account, refreshTopics, chainId],
   )
 
+  const showNotificationModal = useCallback(() => {
+    refreshTopics()
+    toggleSubscribeModal()
+  }, [refreshTopics, toggleSubscribeModal])
+
   return {
     topicGroups,
     isLoading,
-    handleSubscribe,
     userInfo,
-    refreshTopics,
+    handleSubscribe,
+    showNotificationModal,
   }
 }
 
