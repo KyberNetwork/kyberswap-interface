bafeb0665c303ea90c78fe1e6a48f8db0f0a4755
Danh <nguyenhoaidanh2096@gmail.com>
2023-01-16 16:06:58 +0700

    chore

--- src/components/swapv2/LimitOrder/useBaseTradeInfo.ts
+++ src/components/swapv2/LimitOrder/useBaseTradeInfo.ts
@@ -20,6 +20,7 @@ import { tryParseAmount } from 'state/swap/hooks'
 
 type BaseTradeInfo = {
   price: Price<Currency, Currency>
+  invert: Price<Currency, Currency>
   amountInUsd: number
   outputAmount: any
 }
@@ -28,19 +29,19 @@ const MAX_RETRY_COUNT = 2
 
 // 1 knc = ?? usdt
 export default function useBaseTradeInfo(currencyIn: Currency | undefined, currencyOut: Currency | undefined) {
-  const { account, chainId, networkInfo } = useActiveWeb3React()
+  const { account, chainId } = useActiveWeb3React()
   const tokenInAddress = currencyIn?.isNative ? ETHER_ADDRESS : currencyIn?.wrapped.address ?? ''
   const tokenOutAddress = currencyOut?.isNative ? ETHER_ADDRESS : currencyOut?.wrapped.address ?? ''
   const amountIn = tryParseAmount('1', currencyIn)
 
   const mapAmountNative: any = {
-    [ChainId.MATIC]: 1000,
-    [ChainId.MAINNET]: 1,
-    [ChainId.BSCMAINNET]: 5,
-    [ChainId.ARBITRUM]: 1,
-    [ChainId.OPTIMISM]: 1,
-    [ChainId.AVAXMAINNET]: 20,
-    [ChainId.FANTOM]: 3000,
+    [ChainId.MATIC]: '1000',
+    [ChainId.MAINNET]: '1',
+    [ChainId.BSCMAINNET]: '5',
+    [ChainId.ARBITRUM]: '1',
+    [ChainId.OPTIMISM]: '1',
+    [ChainId.AVAXMAINNET]: '20',
+    [ChainId.FANTOM]: '3000',
   }
 
   const getApiUrl = (
@@ -58,14 +59,19 @@ export default function useBaseTradeInfo(currencyIn: Currency | undefined, curre
       : null
   }
 
-  const controller = useRef(new AbortController())
-  const fetchData = async (url: string | null): Promise<BaseTradeInfo | undefined> => {
+  // const controller = useRef(new AbortController())
+  const fetchData = async (
+    url: string | null,
+    amount?: any,
+    currencyA?: any,
+    currencyB?: any,
+  ): Promise<BaseTradeInfo | undefined> => {
     if (!currencyOut || !currencyIn || !url) return
 
-    controller.current.abort()
-    controller.current = new AbortController()
+    // controller.current.abort()
+    // controller.current = new AbortController()
     const data = await fetch(url, {
-      signal: controller.current.signal,
+      // signal: controller.current.signal,
       headers: {
         'X-Request-Id': sentryRequestId,
       },
@@ -74,9 +80,18 @@ export default function useBaseTradeInfo(currencyIn: Currency | undefined, curre
     const { outputAmount, amountInUsd } = data
     const amountOut = TokenAmount.fromRawAmount(currencyOut, JSBI.BigInt(outputAmount))
 
-    if (amountIn?.quotient && amountOut?.quotient) {
+    const customAmount = amount || amountIn
+    console.log(amount, amountIn)
+
+    if (customAmount?.quotient && amountOut?.quotient) {
       return {
-        price: new Price(currencyIn, currencyOut, amountIn?.quotient, amountOut?.quotient),
+        price: new Price(
+          currencyA ?? currencyIn,
+          currencyB ?? currencyOut,
+          customAmount?.quotient,
+          amountOut?.quotient,
+        ),
+        invert: new Price(currencyOut, currencyIn, amountOut?.quotient, customAmount?.quotient),
         amountInUsd,
         outputAmount,
       }
@@ -90,34 +105,55 @@ export default function useBaseTradeInfo(currencyIn: Currency | undefined, curre
     async url => {
       try {
         if (!mapAmountNative[chainId] || !currencyIn || !currencyOut) return
-
-        if (currencyIn.isNative || currencyOut.isToken) {
+        if (currencyIn.isNative) {
           const tokenNotNative = currencyIn.isNative ? currencyOut : currencyIn
           const amountA = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
-          const [data] = await Promise.all([
-            fetchData(getApiUrl(amountA, WETH[chainId].wrapped.address, tokenNotNative.wrapped.address)),
-          ])
-          if (!data) return
+          const customUrl = getApiUrl(amountA, WETH[chainId].wrapped.address, tokenNotNative.wrapped.address)
+          const [data] = await Promise.all([fetchData(customUrl, amountA)])
           retryCount.current = 0
           return data
-        } else {
-          const amountA = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
-          const amountB = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
-          const [a, b] = await Promise.all([
-            fetchData(getApiUrl(amountA, WETH[chainId].wrapped.address, currencyIn.wrapped.address)),
-            fetchData(getApiUrl(amountB, WETH[chainId].wrapped.address, currencyOut.wrapped.address)),
-          ])
-          if (!a || !b) return undefined
-          const outA = TokenAmount.fromRawAmount(currencyIn, JSBI.BigInt(b.outputAmount))
-          const outB = TokenAmount.fromRawAmount(currencyOut, JSBI.BigInt(a.outputAmount))
-          const rate = outB.divide(outA).toFixed(10)
-          console.log('test', rate)
         }
+        if (currencyOut.isNative) {
+          const amountA = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
+          const [a] = await Promise.all([
+            fetchData(
+              getApiUrl(amountA, WETH[chainId].wrapped.address, currencyIn.wrapped.address),
+              amountA,
+              WETH[chainId],
+              currencyIn,
+            ),
+          ])
+          console.log(a)
 
+          retryCount.current = 0
+          return { ...a, price: a?.price.invert() }
+
+          retryCount.current = 0
+          // return currencyIn.isNative ? data : data
+        }
         const data = await fetchData(url)
-
         retryCount.current = 0
         return data
+
+        // // else
+        // const amountA = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
+        // const amountB = tryParseAmount(mapAmountNative[chainId], WETH[chainId])
+        // const [a, b, data] = await Promise.all([
+        //   fetchData(getApiUrl(amountA, WETH[chainId].wrapped.address, currencyIn.wrapped.address)),
+        //   fetchData(getApiUrl(amountB, WETH[chainId].wrapped.address, currencyOut.wrapped.address)),
+        //   fetchData(url),
+        // ])
+        // if (!a || !b) return undefined
+
+        // const outA = TokenAmount.fromRawAmount(currencyIn, JSBI.BigInt(b.outputAmount))
+        // const outB = TokenAmount.fromRawAmount(currencyOut, JSBI.BigInt(a.outputAmount))
+        // const rate = outB.divide(outA).toFixed(10)
+        // console.log('test', rate, data)
+        // retryCount.current = 0
+        // return {
+        //   ...data,
+        //   price: new Price(currencyIn, currencyOut, outA?.quotient, outB?.quotient),
+        // }
       } catch (error) {
         retryCount.current++
         if (retryCount.current <= MAX_RETRY_COUNT) {
