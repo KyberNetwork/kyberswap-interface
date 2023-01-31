ed28325e56d3e0141b9e796ff60caa536e03bc96
Doan Sy Hung <hungdoansy@gmail.com>
2023-01-31 14:33:08 +0700

    chore: remove 1px glitch in card background

--- src/components/WalletPopup/AccountInfo/CardBackground.tsx
+++ src/components/WalletPopup/AccountInfo/CardBackground.tsx
@@ -15,7 +15,6 @@ const BackgroundWrapper = styled.div`
 
   box-shadow: inset 0px 1px 1px rgba(255, 255, 255, 0.15), inset -1px -1px 1px rgba(0, 0, 0, 0.08);
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  backdrop-filter: blur(4px);
   mix-blend-mode: overlay;
   border-radius: 20px;
   overflow: hidden;
