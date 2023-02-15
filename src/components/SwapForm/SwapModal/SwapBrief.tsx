f00847e3f413c6f8ed9dc57250234a45085e4e4c
Doan Sy Hung <hungdoansy@gmail.com>
2023-02-15 14:35:28 +0700

    wip: new flow

--- src/components/SwapForm/SwapModal/SwapBrief.tsx
+++ src/components/SwapForm/SwapModal/SwapBrief.tsx
@@ -1,54 +1,75 @@
 import { Currency, CurrencyAmount } from '@kyberswap/ks-sdk-core'
 import React from 'react'
 import { ArrowDown } from 'react-feather'
-import { Text } from 'rebass'
+import { Flex, Text } from 'rebass'
+import styled from 'styled-components'
 
 import { AutoColumn } from 'components/Column'
 import CurrencyLogo from 'components/CurrencyLogo'
-import { RowBetween, RowFixed } from 'components/Row'
+import { RowFixed } from 'components/Row'
 import { useSwapFormContext } from 'components/SwapForm/SwapFormContext'
-import { TruncatedText } from 'components/swapv2/styleds'
+import UpdatedBadge from 'components/SwapForm/SwapModal/SwapDetailsv2/UpdatedBadge'
 import useTheme from 'hooks/useTheme'
 
 type Props = {
   inputAmount: CurrencyAmount<Currency>
   outputAmount: CurrencyAmount<Currency>
+  levelOfChanges: number | undefined
 }
 
-const SwapBrief: React.FC<Props> = ({ inputAmount, outputAmount }) => {
+const TruncatedText = styled(Text)`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-size: 24px;
+  font-weight: 500;
+`
+
+const SwapBrief: React.FC<Props> = ({ inputAmount, outputAmount, levelOfChanges }) => {
   const theme = useTheme()
   const { typedValue, feeConfig } = useSwapFormContext()
   return (
-    <AutoColumn gap={'md'} style={{ marginTop: '20px' }}>
-      <RowBetween align="flex-end" justify="flex-start">
-        <RowFixed gap={'0px'}>
-          <CurrencyLogo currency={inputAmount.currency} size={'24px'} style={{ marginRight: '12px' }} />
-          <TruncatedText fontSize={24} fontWeight={500}>
-            {!!feeConfig ? typedValue : inputAmount.toSignificant(6)}
-          </TruncatedText>
-        </RowFixed>
-        <RowFixed gap={'0px'}>
-          <Text fontSize={24} fontWeight={500} style={{ marginLeft: '10px' }}>
-            {inputAmount.currency.symbol}
-          </Text>
-        </RowFixed>
-      </RowBetween>
+    <AutoColumn gap={'md'} style={{ marginTop: '20px', padding: '0 8px' }}>
+      <Flex
+        alignItems="center"
+        width="100%"
+        sx={{
+          gap: '8px',
+        }}
+      >
+        <CurrencyLogo currency={inputAmount.currency} size={'24px'} />
+        <TruncatedText>{!!feeConfig ? typedValue : inputAmount.toSignificant(6)}</TruncatedText>
+        <Text flex="0 0 fit-content" fontSize={24} fontWeight={500}>
+          {inputAmount.currency.symbol}
+        </Text>
+      </Flex>
+
       <RowFixed>
         <ArrowDown size="16" color={theme.text2} style={{ marginLeft: '4px', minWidth: '16px' }} />
       </RowFixed>
-      <RowBetween align="flex-end" justify="flex-start">
-        <RowFixed gap={'0px'}>
-          <CurrencyLogo currency={outputAmount?.currency} size={'24px'} style={{ marginRight: '12px' }} />
-          <TruncatedText fontSize={24} fontWeight={500}>
-            {outputAmount.toSignificant(6) || ''}
-          </TruncatedText>
-        </RowFixed>
-        <RowFixed gap={'0px'}>
-          <Text fontSize={24} fontWeight={500} style={{ marginLeft: '10px' }}>
+
+      <Flex
+        alignItems="center"
+        width="100%"
+        sx={{
+          gap: '8px',
+        }}
+      >
+        <Flex
+          alignItems="center"
+          flex="1 1 0"
+          sx={{
+            gap: '8px',
+          }}
+        >
+          <CurrencyLogo currency={outputAmount?.currency} size={'24px'} />
+          <TruncatedText>{outputAmount.toSignificant(6) || ''}</TruncatedText>
+          <Text flex="0 0 fit-content" fontSize={24} fontWeight={500}>
             {outputAmount.currency.symbol}
           </Text>
-        </RowFixed>
-      </RowBetween>
+        </Flex>
+
+        <UpdatedBadge level={levelOfChanges} />
+      </Flex>
     </AutoColumn>
   )
 }
