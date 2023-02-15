0ee5d658508dcc2fc9fa4930ad3ba480777ae7b2
Doan Sy Hung <hungdoansy@gmail.com>
2023-02-15 10:50:06 +0700

    wip: new swap confirmation flow

--- src/components/SwapForm/SwapModal/ConfirmSwapModalContent.tsx
+++ src/components/SwapForm/SwapModal/ConfirmSwapModalContent.tsx
@@ -1,19 +1,14 @@
 import { Price } from '@kyberswap/ks-sdk-core'
 import { Trans } from '@lingui/macro'
-import { rgba } from 'polished'
 import React, { useEffect, useState } from 'react'
-import { AlertTriangle } from 'react-feather'
-import { Flex, Text } from 'rebass'
-import { calculatePriceImpact } from 'services/route/utils'
+import { Text } from 'rebass'
 import styled from 'styled-components'
 
 import { ButtonPrimary } from 'components/Button'
 import { GreyCard } from 'components/Card'
 import { AutoColumn } from 'components/Column'
-import Loader from 'components/Loader'
 import { AutoRow, RowBetween } from 'components/Row'
 import { useSwapFormContext } from 'components/SwapForm/SwapFormContext'
-import AcceptChangesNotice from 'components/SwapForm/SwapModal/AcceptChangesNotice'
 import { BuildRouteResult } from 'components/SwapForm/hooks/useBuildRoute'
 import { Dots } from 'components/swapv2/styleds'
 import { useActiveWeb3React } from 'hooks'
@@ -21,10 +16,9 @@ import useTheme from 'hooks/useTheme'
 import { useEncodeSolana } from 'state/swap/hooks'
 import { CloseIcon } from 'theme/components'
 import { toCurrencyAmount } from 'utils/currencyAmount'
-import { checkPriceImpact } from 'utils/prices'
 
 import SwapBrief from './SwapBrief'
-import SwapDetails, { Props as SwapDetailsProps } from './SwapDetails'
+import SwapDetails, { Props as SwapDetailsProps } from './SwapDetailsv2'
 
 const Wrapper = styled.div`
   display: flex;
@@ -62,23 +56,22 @@ const ConfirmSwapModalContent: React.FC<Props> = ({
     !isBuildingRoute && !errorWhileBuildRoute && !isAcceptedChanges && buildResult?.data?.outputChange?.level !== 0
   const shouldDisableConfirmButton = isBuildingRoute || !!errorWhileBuildRoute || shouldShowAcceptChanges
 
-  const priceImpactCheck = checkPriceImpact(routeSummary?.priceImpact)
-
-  const handleAcceptChanges = () => {
-    setAcceptedChanges(true)
-  }
-
   const getSwapDetailsProps = (): SwapDetailsProps => {
-    if (!buildResult?.data || !routeSummary || !isAcceptedChanges) {
+    if (!buildResult?.data || !routeSummary) {
       return {
-        acceptedChanges: undefined,
+        isLoading: isBuildingRoute,
+        hasError: !!errorWhileBuildRoute,
+
+        gasUsd: undefined,
+        executionPrice: undefined,
+        parsedAmountOut: undefined,
+        amountInUsd: undefined,
       }
     }
 
-    const { amountIn, amountInUsd, amountOut, amountOutUsd, gasUsd } = buildResult.data
+    const { amountIn, amountInUsd, amountOut, gasUsd } = buildResult.data
     const parsedAmountIn = toCurrencyAmount(routeSummary.parsedAmountIn.currency, amountIn)
     const parsedAmountOut = toCurrencyAmount(routeSummary.parsedAmountOut.currency, amountOut)
-    const priceImpact = calculatePriceImpact(Number(amountInUsd), Number(amountOutUsd))
     const executionPrice = new Price(
       parsedAmountIn.currency,
       parsedAmountOut.currency,
@@ -87,13 +80,13 @@ const ConfirmSwapModalContent: React.FC<Props> = ({
     )
 
     return {
-      acceptedChanges: {
-        gasUsd,
-        priceImpact,
-        executionPrice,
-        parsedAmountOut,
-        amountInUsd,
-      },
+      isLoading: isBuildingRoute,
+      hasError: !!errorWhileBuildRoute,
+
+      gasUsd,
+      executionPrice,
+      parsedAmountOut,
+      amountInUsd,
     }
   }
 
@@ -131,91 +124,29 @@ const ConfirmSwapModalContent: React.FC<Props> = ({
         {renderSwapBrief()}
       </AutoColumn>
 
-      {shouldShowAcceptChanges && (
-        <AcceptChangesNotice level={buildResult?.data?.outputChange?.level || 0} onAcceptChange={handleAcceptChanges} />
-      )}
+      <SwapDetails {...getSwapDetailsProps()} />
 
-      {!isBuildingRoute && errorWhileBuildRoute ? (
-        <Flex
-          sx={{
-            width: '100%',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: '16px',
-          }}
-        >
-          <Flex
-            sx={{
-              width: '100%',
-              padding: '12px 16px',
-              alignItems: 'center',
-              borderRadius: '8px',
-              background: rgba(theme.warning, 0.2),
-            }}
-            color={theme.warning}
-          >
-            <AlertTriangle size={16} style={{ marginRight: '10px', flex: '0 0 16px' }} />
-            <Text fontSize="12px" fontWeight="500">
-              {errorWhileBuildRoute}
-            </Text>
-          </Flex>
+      <AutoRow>
+        {isSolana && !encodeSolana ? (
+          <GreyCard style={{ textAlign: 'center', borderRadius: '999px', padding: '12px' }} id="confirm-swap-or-send">
+            <Dots>
+              <Trans>Checking accounts</Trans>
+            </Dots>
+          </GreyCard>
+        ) : errorWhileBuildRoute ? (
           <ButtonPrimary onClick={onRetry}>
             <Text fontSize={14} fontWeight={500} as="span" lineHeight={1}>
               <Trans>Try again</Trans>
             </Text>
           </ButtonPrimary>
-        </Flex>
-      ) : (
-        <>
-          <AutoColumn gap="0">
-            {isBuildingRoute ? (
-              <Flex
-                sx={{
-                  height: '160px',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Loader size="48px" stroke={theme.primary} strokeWidth="1" />
-              </Flex>
-            ) : (
-              <SwapDetails {...getSwapDetailsProps()} />
-            )}
-          </AutoColumn>
-
-          <AutoRow>
-            {isSolana && !encodeSolana ? (
-              <GreyCard
-                style={{ textAlign: 'center', borderRadius: '999px', padding: '12px' }}
-                id="confirm-swap-or-send"
-              >
-                <Dots>
-                  <Trans>Checking accounts</Trans>
-                </Dots>
-              </GreyCard>
-            ) : (
-              <ButtonPrimary
-                onClick={onSwap}
-                disabled={shouldDisableConfirmButton}
-                style={
-                  priceImpactCheck.isHigh && !shouldDisableConfirmButton
-                    ? {
-                        border: 'none',
-                        background: priceImpactCheck.isVeryHigh ? theme.red : theme.warning,
-                        color: theme.white,
-                      }
-                    : undefined
-                }
-                id="confirm-swap-or-send"
-              >
-                <Text fontSize={14} fontWeight={500} as="span" lineHeight={1}>
-                  <Trans>Confirm Swap</Trans>
-                </Text>
-              </ButtonPrimary>
-            )}
-          </AutoRow>
-        </>
-      )}
+        ) : (
+          <ButtonPrimary onClick={onSwap} disabled={shouldDisableConfirmButton} id="confirm-swap-or-send">
+            <Text fontSize={14} fontWeight={500} as="span" lineHeight={1}>
+              <Trans>Confirm Swap</Trans>
+            </Text>
+          </ButtonPrimary>
+        )}
+      </AutoRow>
     </Wrapper>
   )
 }
