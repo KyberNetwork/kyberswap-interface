ace671dca290fe65a856ab936fa8973a20804ec2
nguyenhuudungz <34548156+nguyenhuudungz@users.noreply.github.com>
2023-02-21 09:44:17 +0700

    [WIP] New swap flow with 2-api, path /swap-v3

--- src/pages/SwapV3/redirects.tsx
+++ src/pages/SwapV3/redirects.tsx
@@ -14,10 +14,8 @@ export function RedirectPathToSwapV3Network() {
 
   if (pathname.startsWith(APP_PATHS.LIMIT) && getLimitOrderContract(chainId)) {
     redirectTo = APP_PATHS.LIMIT
-  } else if (pathname.startsWith(APP_PATHS.SWAP_V3)) {
-    redirectTo = APP_PATHS.SWAP_V3
   } else {
-    redirectTo = APP_PATHS.SWAP
+    redirectTo = APP_PATHS.SWAP_V3
   }
 
   return (
