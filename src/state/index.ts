COSVM-NETWORK/FSwap

refactor: switch to use rtk query

src/state/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { load, save } from 'redux-localstorage-simple'
+import routeApi from 'services/route'
 
 import { ENV_LEVEL } from 'constants/env'
 import { ENV_TYPE } from 'constants/type'
@@ -60,12 +61,14 @@ const store = configureStore({
     elasticFarm,
     tokenPrices,
     topTokens,
+    routeApi: routeApi.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({ thunk: true, immutableCheck: false, serializableCheck: false })
-      // .concat(dataApi.middleware)
+      .concat(routeApi.middleware)
       .concat(save({ states: PERSISTED_KEYS, debounce: 100 }))
       .concat(geckoTerminalApi.middleware),
+
   preloadedState: load({ states: PERSISTED_KEYS }),
 })
 

KyberNetwork/kyberswap-interface

src/state/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { load, save } from 'redux-localstorage-simple'
+import routeApi from 'services/route'
 
 import { ENV_LEVEL } from 'constants/env'
 import { ENV_TYPE } from 'constants/type'
@@ -63,13 +64,16 @@ const store = configureStore({
     elasticFarm,
     tokenPrices,
     topTokens,
+    routeApi: routeApi.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({ thunk: true, immutableCheck: false, serializableCheck: false })
+      // .concat(dataApi.middleware)
       .concat(save({ states: PERSISTED_KEYS, debounce: 100 }))
       .concat(geckoTerminalApi.middleware)
       .concat(ksSettingApi.middleware)
-      .concat(annoucementApi.middleware),
+      .concat(annoucementApi.middleware)
+      .concat(routeApi.middleware),
   preloadedState: load({ states: PERSISTED_KEYS }),
 })
 