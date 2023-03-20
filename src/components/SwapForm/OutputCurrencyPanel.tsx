62ae0ead542585484510a68ae901fd470a3ff1f2
Nam Nguyen <nam.nguyen@kyber.network>
2023-03-20 07:20:56 +0700

    fix(swap): show output more precise

--- src/components/SwapForm/OutputCurrencyPanel.tsx
+++ src/components/SwapForm/OutputCurrencyPanel.tsx
@@ -30,8 +30,8 @@ const OutputCurrencyPanel: React.FC<Props> = ({
     if (showWrap) {
       return parsedAmountIn?.toExact() || ''
     }
-
-    return parsedAmountOut?.toSignificant(6) || ''
+    if (!parsedAmountOut) return ''
+    return Number(parsedAmountOut.toFixed(currencyOut?.decimals ?? 10)).toString()
   }
 
   const getEstimatedUsd = () => {
