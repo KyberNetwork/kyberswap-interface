1a8e5ad68b57af4e48fa6c33c72aae671854c13d
Doan Sy Hung <hungdoansy@gmail.com>
2023-03-20 14:14:33 +0700

    add empty, loading state for price alerts

--- src/pages/NotificationCenter/PriceAlerts/index.tsx
+++ src/pages/NotificationCenter/PriceAlerts/index.tsx
@@ -1,10 +1,14 @@
+import { Trans } from '@lingui/macro'
 import { useState } from 'react'
-import { Flex } from 'rebass'
+import { Info } from 'react-feather'
+import { Flex, Text } from 'rebass'
 import { useGetListAlertsQuery } from 'services/priceAlert'
 import styled from 'styled-components'
 
+import Loader from 'components/Loader'
 import Pagination from 'components/Pagination'
 import { useActiveWeb3React } from 'hooks'
+import useTheme from 'hooks/useTheme'
 import Header from 'pages/NotificationCenter/PriceAlerts/Header'
 
 import SingleAlert from './SingleAlert'
@@ -17,6 +21,7 @@ const CustomPagination = styled(Pagination)`
 const ITEMS_PER_PAGE = 10
 
 const PriceAlerts = () => {
+  const theme = useTheme()
   const { account } = useActiveWeb3React()
   const [page, setPage] = useState(1)
   const { data, isLoading } = useGetListAlertsQuery({
@@ -26,38 +31,60 @@ const PriceAlerts = () => {
   })
 
   if (isLoading) {
-    return <div>loading</div>
-  }
-
-  if (!data?.alerts) {
-    return null
+    return (
+      <Flex justifyContent="center" height="100%" minHeight="100%" alignItems="center">
+        <Loader size="36px" />
+      </Flex>
+    )
   }
 
   return (
     <Flex
       sx={{
+        width: '100%',
+        height: '100%',
         flexDirection: 'column',
       }}
     >
       <Header />
 
-      <Flex
-        sx={{
-          flexDirection: 'column',
-        }}
-      >
-        {data.alerts.map(alert => {
-          return <SingleAlert key={alert.id} alert={alert} />
-        })}
-      </Flex>
+      {!data?.alerts?.length ? (
+        <Flex flex="1 1 0" justifyContent="center" width="100%" alignItems="center">
+          <Flex
+            sx={{
+              flexDirection: 'column',
+              alignItems: 'center',
+              color: theme.subText,
+              gap: '0.75rem',
+            }}
+          >
+            <Info size={'24px'} />
+            <Text as="span">
+              <Trans>No price alerts created yet</Trans>
+            </Text>
+          </Flex>
+        </Flex>
+      ) : (
+        <>
+          <Flex
+            sx={{
+              flexDirection: 'column',
+            }}
+          >
+            {data.alerts.map(alert => {
+              return <SingleAlert key={alert.id} alert={alert} />
+            })}
+          </Flex>
 
-      <CustomPagination
-        onPageChange={setPage}
-        totalCount={data?.pagination?.totalItems || 0}
-        currentPage={page}
-        pageSize={ITEMS_PER_PAGE}
-        haveBg={false}
-      />
+          <CustomPagination
+            onPageChange={setPage}
+            totalCount={data?.pagination?.totalItems || 0}
+            currentPage={page}
+            pageSize={ITEMS_PER_PAGE}
+            haveBg={false}
+          />
+        </>
+      )}
     </Flex>
   )
 }
