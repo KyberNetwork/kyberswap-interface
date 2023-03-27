13fe3d00376362ed7baa24efccb7e4f351264e9c
Nam Nguyen <nam.nguyen@kyber.network>
2023-03-27 16:03:26 +0700

    fix: show kyberswap limit order dexes by ks-setting result

--- src/state/customizeDexes/updater.tsx
+++ src/state/customizeDexes/updater.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 
-import { kyberswapDexes } from 'constants/dexes'
-import { ELASTIC_NOT_SUPPORTED } from 'constants/v2'
+import { KYBERSWAP_KS_DEXES_TO_UI_DEXES, KYBERSWAP_UI_DEXES } from 'constants/dexes'
 import { useActiveWeb3React } from 'hooks'
 import useLiquiditySources from 'hooks/useAggregatorStats'
 import { AppDispatch } from 'state/index'
@@ -12,19 +11,25 @@ import { updateAllDexes } from '.'
 export default function Updater(): null {
   const dispatch = useDispatch<AppDispatch>()
 
-  const { chainId, isEVM } = useActiveWeb3React()
+  const { chainId } = useActiveWeb3React()
   const { data: dexes } = useLiquiditySources(chainId)
 
   // filterout kyberswap dexes, will hardcode
   const normalizeDexes = useMemo(() => {
-    const dexesOutsideKyberswap =
-      dexes?.map(item => ({ ...item, id: item.dexId })).filter(item => !item.dexId.includes('kyberswap')) || []
-    const isSupportKSElastic = !ELASTIC_NOT_SUPPORTED[chainId]
-    return [
-      ...dexesOutsideKyberswap,
-      ...(isEVM ? kyberswapDexes.filter(item => isSupportKSElastic || item.id !== 'kyberswapv2') : []),
-    ]
-  }, [dexes, chainId, isEVM])
+    const dexesFormatted = dexes?.map(item => ({ ...item, id: item.dexId })) || []
+    const dexesOutsideKyberswap = dexesFormatted.filter(item => !item.dexId.includes('kyberswap'))
+    const dexesKyberswap = (
+      [
+        ...new Set(
+          dexesFormatted
+            .filter(item => item.dexId.includes('kyberswap'))
+            .map(dex => KYBERSWAP_KS_DEXES_TO_UI_DEXES[dex.id]),
+        ),
+      ].filter(Boolean) as string[]
+    ).map(uiDexKey => KYBERSWAP_UI_DEXES[uiDexKey])
+
+    return [...dexesOutsideKyberswap, ...dexesKyberswap]
+  }, [dexes])
 
   useEffect(() => {
     if (chainId && normalizeDexes.length) {
