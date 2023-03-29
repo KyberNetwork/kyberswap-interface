bce6022cd8ed78fde6716c1b61d49af43fa86c6a
Danh <nguyenhoaidanh2096@gmail.com>
2023-03-29 09:42:37 +0700

    refresh topic after save

--- src/components/SubscribeButton/NotificationPreference/index.tsx
+++ src/components/SubscribeButton/NotificationPreference/index.tsx
@@ -333,6 +333,9 @@ function NotificationPreference({
         10000,
       )
       toggleModal()
+      if (isInNotificationCenter) {
+        refreshTopics()
+      }
     } catch (error) {
       notify({
         title: t`Save Error`,
