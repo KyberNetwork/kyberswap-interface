66a8f4b3900f6aeb43f2ba20d9ab1a2ecbd3a277
Danh <nguyenhoaidanh2096@gmail.com>
2023-03-30 14:49:07 +0700

    trans

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,4 @@
+import { t } from '@lingui/macro'
 import dayjs from 'dayjs'
 
 // ex: 60 => 1 minute
@@ -8,10 +9,10 @@ export const formatTimeDuration = (t: number) => {
 export const formatTime = (time: number) => {
   const delta = (Date.now() - time * 1000) / 1000
   const min = Math.floor(delta / 60)
-  if (min < 1) return `< 1 minute ago`
-  if (min < 60) return `${min} minutes ago`
+  if (min < 1) return t`< 1 minute ago`
+  if (min < 60) return t`${min} minutes ago`
   const hour = Math.floor(delta / 3600)
-  if (hour < 24) return `${hour} hours ago`
+  if (hour < 24) return t`${hour} hours ago`
   const day = Math.floor(delta / (24 * 3600))
-  return `${day} days ago`
+  return t`${day} days ago`
 }
