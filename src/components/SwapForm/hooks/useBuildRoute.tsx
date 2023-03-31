0b348e9f47b9848e937e0b3ecfbbc0609a4b1b15
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2023-03-31 11:30:02 +0700

    More

--- src/components/SwapForm/hooks/useBuildRoute.tsx
+++ src/components/SwapForm/hooks/useBuildRoute.tsx
@@ -60,8 +60,8 @@ const useBuildRoute = (args: Args) => {
       abortControllerRef.current.abort()
       abortControllerRef.current = new AbortController()
 
-      // const url = `${aggregatorDomain}/${NETWORKS_INFO[chainId].aggregatorRoute}/api/v1/route/build`
-      const url = `http://localhost:8014/${NETWORKS_INFO[chainId].aggregatorRoute}/api/v1/route/build`
+      const url = `${aggregatorDomain}/${NETWORKS_INFO[chainId].aggregatorRoute}/api/v1/route/build`
+      // const url = `http://localhost:8014/${NETWORKS_INFO[chainId].aggregatorRoute}/api/v1/route/build`
       const response = await buildRoute(url, payload, abortControllerRef.current.signal)
       // await new Promise(resolve => setTimeout(() => resolve(undefined), 60000))
 
