2d3c0ca340caef713415bda3e17a3c96e0e9b44f
Doan Sy Hung <hungdoansy@gmail.com>
2023-04-04 09:49:12 +0700

    add tooltip if receive anyToken

--- src/pages/Bridge/BridgeTransferHistory/TokenReceiveCell.tsx
+++ src/pages/Bridge/BridgeTransferHistory/TokenReceiveCell.tsx
@@ -1,24 +1,45 @@
-import { Text } from 'rebass'
+import { Info } from 'react-feather'
+import { Flex, Text } from 'rebass'
 
+import { MouseoverTooltip } from 'components/Tooltip'
 import { MultichainTransfer } from 'hooks/bridge/useGetBridgeTransfers'
+import useTheme from 'hooks/useTheme'
 import { formatAmountBridge } from 'pages/Bridge/helpers'
+import { ExternalLink } from 'theme'
 
 type Props = {
   transfer: MultichainTransfer
 }
 
 const TokenReceiveCell: React.FC<Props> = ({ transfer }) => {
+  const theme = useTheme()
+
+  const tooltipText = (
+    <Text>
+      You have received some anyToken from Multichain. You can exchange your anyToken to {transfer.dstTokenSymbol} at{' '}
+      Multichain, when the pool has sufficient liquidity.{' '}
+      <ExternalLink href="https://app.multichain.org/#/pool">See here ↗</ExternalLink>
+    </Text>
+  )
   return (
-    <Text
-      as="span"
+    <Flex
       sx={{
+        alignItems: 'center',
+        flexWrap: 'nowrap',
         fontWeight: 500,
         fontSize: '12px',
         lineHeight: '16px',
+        gap: '4px',
+        color: transfer.isReceiveAnyToken ? theme.warning : undefined,
       }}
     >
-      {formatAmountBridge(transfer.dstAmount)} {transfer.dstTokenSymbol}
-    </Text>
+      {formatAmountBridge(transfer.dstAmount)} {transfer.dstTokenSymbol}{' '}
+      {transfer.isReceiveAnyToken && (
+        <MouseoverTooltip text={tooltipText} placement="top">
+          <Info size={16} />
+        </MouseoverTooltip>
+      )}
+    </Flex>
   )
 }
 
