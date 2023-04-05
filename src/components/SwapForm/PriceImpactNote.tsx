fc0b70618b866502821e75f153ebafef0faff708
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2023-04-05 10:07:59 +0700

    Fix

--- src/components/SwapForm/PriceImpactNote.tsx
+++ src/components/SwapForm/PriceImpactNote.tsx
@@ -4,18 +4,20 @@ import { Text } from 'rebass'
 import styled from 'styled-components'
 
 import Row from 'components/Row'
-import { MouseoverTooltip } from 'components/Tooltip'
 import WarningNote from 'components/WarningNote'
 import { checkPriceImpact } from 'utils/prices'
 
-const TextDashedColor = styled(Text)`
-  border-bottom: 1px dashed ${({ theme }) => theme.text};
+const TextUnderlineColor = styled(Text)`
+  border-bottom: 1px solid ${({ theme }) => theme.text};
   width: fit-content;
+  cursor: pointer;
+  color: ${({ theme }) => theme.text};
 `
 
-const TextDashedTransparent = styled(Text)`
-  border-bottom: 1px dashed transparent;
+const TextUnderlineTransparent = styled(Text)`
+  border-bottom: 1px solid transparent;
   width: fit-content;
+  cursor: pointer;
 `
 
 const PRICE_IMPACT_EXPLANATION_URL =
@@ -70,33 +72,13 @@ const PriceImpactNote: FC<Props> = ({ isDegenMode, priceImpact }) => {
         shortText={
           <Row alignItems="center" style={{ gap: '0.5ch' }}>
             <Trans>
-              <TextDashedColor>
-                <MouseoverTooltip
-                  placement="top"
-                  width="fit-content"
-                  text={
-                    <Trans>
-                      <Text fontSize={12}>
-                        Read more{' '}
-                        <a
-                          href={PRICE_IMPACT_EXPLANATION_URL}
-                          target="_blank"
-                          rel="noreferrer"
-                          onClick={e => e.stopPropagation()}
-                        >
-                          <b>here ↗</b>
-                        </a>
-                      </Text>
-                    </Trans>
-                  }
-                >
-                  Price Impact
-                </MouseoverTooltip>
-              </TextDashedColor>
-              <TextDashedTransparent>
+              <TextUnderlineColor as="a" href={PRICE_IMPACT_EXPLANATION_URL} target="_blank" rel="noreferrer">
+                Price Impact
+              </TextUnderlineColor>
+              <TextUnderlineTransparent>
                 {' '}
                 is <b>very</b> high
-              </TextDashedTransparent>
+              </TextUnderlineTransparent>
             </Trans>
           </Row>
         }
@@ -124,25 +106,10 @@ const PriceImpactNote: FC<Props> = ({ isDegenMode, priceImpact }) => {
   const shortText = (
     <Row alignItems="center" style={{ gap: '0.5ch' }}>
       <Trans>
-        <TextDashedColor>
-          <MouseoverTooltip
-            placement="top"
-            width="fit-content"
-            text={
-              <Trans>
-                <Text fontSize={12}>
-                  Read more{' '}
-                  <a href={PRICE_IMPACT_EXPLANATION_URL} target="_blank" rel="noreferrer">
-                    <b>here ↗</b>
-                  </a>
-                </Text>
-              </Trans>
-            }
-          >
-            Price Impact
-          </MouseoverTooltip>
-        </TextDashedColor>
-        <TextDashedTransparent> is high.</TextDashedTransparent>
+        <TextUnderlineColor as="a" href={PRICE_IMPACT_EXPLANATION_URL} target="_blank" rel="noreferrer">
+          Price Impact
+        </TextUnderlineColor>
+        <TextUnderlineTransparent> is high.</TextUnderlineTransparent>
       </Trans>
     </Row>
   )
