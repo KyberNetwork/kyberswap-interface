e6cbb3eefd8e34a2059edf9fabf9e6a7b7f1a5c5
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2023-04-12 09:38:42 +0700

    Finish

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,12 +7,12 @@ import { Web3ReactContextInterface } from '@web3-react/core/dist/types'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { isMobile } from 'react-device-detect'
 import { useSelector } from 'react-redux'
+import { useSearchParams } from 'react-router-dom'
 
 import { injected, walletconnect, walletlink } from 'connectors'
 import { NETWORKS_INFO } from 'constants/networks'
 import { NetworkInfo } from 'constants/networks/type'
 import { SUPPORTED_WALLET, SUPPORTED_WALLETS, WALLETLINK_LOCALSTORAGE_NAME } from 'constants/wallets'
-import useParsedQueryString from 'hooks/useParsedQueryString'
 import { AppState } from 'state'
 import { useKyberSwapConfig } from 'state/application/hooks'
 import { useIsAcceptedTerm, useIsUserManuallyDisconnect } from 'state/user/hooks'
@@ -102,7 +102,7 @@ export function useActiveWeb3React(): {
 }
 
 export function useWeb3React(key?: string): Web3ReactContextInterface<Web3Provider> & { chainId?: ChainId } {
-  const qs = useParsedQueryString()
+  const [searchParams] = useSearchParams()
   const { connector, library, chainId, account, active, error, activate, setError, deactivate } = useWeb3ReactCore(key)
   const { provider } = useKyberSwapConfig()
 
@@ -119,7 +119,7 @@ export function useWeb3React(key?: string): Web3ReactContextInterface<Web3Provid
     connector,
     library: library || provider,
     chainId: chainId || ChainId.MAINNET,
-    account: qs['account'] || account,
+    account: searchParams.get('account') || account,
     active,
     error,
     activate: activateWrapped,
@@ -160,7 +160,7 @@ export function useEagerConnect() {
 
   useEffect(() => {
     try {
-      // If not accepted Terms or Terms changed: block eager connect for EVM wallets and disconnect manualy for Solana wallet
+      // If not accepted Terms or Terms changed: block eager connect for EVM wallets and disconnect manually for Solana wallet
       if (!isAcceptedTerm) {
         setTried(true)
         disconnect()
