1a6a018b09f4797e8e88731ae155a49cf9d77b25
Danh <nguyenhoaidanh2096@gmail.com>
2023-04-17 17:32:38 +0700

    prevent long msg

--- src/components/Announcement/PrivateAnnoucement/InboxItemPrivateMessage.tsx
+++ src/components/Announcement/PrivateAnnoucement/InboxItemPrivateMessage.tsx
@@ -1,16 +1,30 @@
+import styled from 'styled-components'
+
 import { PrivateAnnouncementProp } from 'components/Announcement/PrivateAnnoucement'
 import InboxIcon from 'components/Announcement/PrivateAnnoucement/Icon'
-import {
-  Dot,
-  InboxItemRow,
-  InboxItemWrapper,
-  PrimaryText,
-  RowItem,
-  Title,
-} from 'components/Announcement/PrivateAnnoucement/styled'
+import { Dot, InboxItemRow, InboxItemWrapper, RowItem, Title } from 'components/Announcement/PrivateAnnoucement/styled'
 import { useNavigateToUrl } from 'components/Announcement/helper'
 import { AnnouncementTemplatePopup } from 'components/Announcement/type'
+import { escapeScriptHtml } from 'utils/string'
 
+const Desc = styled.div`
+  font-size: 12px;
+  color: ${({ theme }) => theme.subText};
+  word-break: break-word;
+  display: block;
+  display: -webkit-box;
+  max-width: 100%;
+  line-height: 16px;
+  height: 34px;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  > * {
+    margin: 0;
+  }
+`
 function InboxItemBridge({
   announcement,
   onRead,
@@ -38,7 +52,7 @@ function InboxItemBridge({
       </InboxItemRow>
 
       <InboxItemRow>
-        <PrimaryText>{templateBody.content}</PrimaryText>
+        <Desc dangerouslySetInnerHTML={{ __html: escapeScriptHtml(templateBody.content ?? '') }} />
       </InboxItemRow>
 
       <InboxItemRow>
