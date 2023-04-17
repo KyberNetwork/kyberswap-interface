a8fb439453816c4272c8594d6d8995192c69fc5b
Doan Sy Hung <hungdoansy@gmail.com>
2023-04-17 11:27:45 +0700

    move pools routes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -241,10 +241,7 @@ export default function App() {
                     )}
 
                     <Route path={`${APP_PATHS.FIND_POOL}`} element={<PoolFinder />} />
-                    <Route path={`${APP_PATHS.POOLS}/:network`} element={<Pools />} />
-                    <Route path={`${APP_PATHS.POOLS}/:network/:currencyIdA`} element={<Pools />} />
-                    <Route path={`${APP_PATHS.POOLS}`} element={<RedirectPathToPoolsNetwork />} />
-                    <Route path={`${APP_PATHS.POOLS}/:network/:currencyIdA/:currencyIdB`} element={<Pools />} />
+
                     <Route path={`${APP_PATHS.FARMS}/:network`} element={<Farm />} />
                     <Route path={`${APP_PATHS.FARMS}`} element={<RedirectPathToFarmNetwork />} />
                     <Route path={`${APP_PATHS.MY_POOLS}/:network`} element={<Pool />} />
@@ -285,6 +282,14 @@ export default function App() {
                     <Route path={`${APP_PATHS.NOTIFICATION_CENTER}/*`} element={<NotificationCenter />} />
                     <Route path={`${APP_PATHS.GRANT_PROGRAMS}`} element={<GrantProgramPage />} />
                     <Route path={`${APP_PATHS.GRANT_PROGRAMS}/:slug`} element={<GrantProgramPage />} />
+
+                    <>
+                      {/* Pools Routes  */}
+                      <Route path={`${APP_PATHS.POOLS}/:network`} element={<Pools />} />
+                      <Route path={`${APP_PATHS.POOLS}/:network/:currencyIdA`} element={<Pools />} />
+                      <Route path={`${APP_PATHS.POOLS}`} element={<RedirectPathToPoolsNetwork />} />
+                      <Route path={`${APP_PATHS.POOLS}/:network/:currencyIdA/:currencyIdB`} element={<Pools />} />
+                    </>
 
                     <>
                       {/* Classic Add Liquidity Routes  */}
