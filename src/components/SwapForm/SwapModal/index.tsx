423a909ec3b3b9ffb9f04bcdb26b0ecf8803474d
Diep Pham <xiaoyhun@gmail.com>
2023-04-20 15:20:30 +0700

    permit fail for old aggregator

--- src/components/SwapForm/SwapModal/index.tsx
+++ src/components/SwapForm/SwapModal/index.tsx
@@ -85,7 +85,12 @@ const SwapModal: React.FC<Props> = props => {
   }
 
   const handleErrorDismiss = () => {
-    if (buildResult?.error && buildResult.error.toLowerCase().includes('permit') && routeSummary && account) {
+    if (
+      ((buildResult?.error && buildResult.error.toLowerCase().includes('permit')) ||
+        (error && error.toLowerCase().includes('permit'))) &&
+      routeSummary &&
+      account
+    ) {
       dispatch(permitError({ chainId, address: routeSummary.parsedAmountIn.currency.wrapped.address, account }))
     }
     handleDismiss()
