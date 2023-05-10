022f47f3bd71547e4f6acaf97df0de5595daff1d
Danh <nguyenhoaidanh2096@gmail.com>
2023-05-10 13:07:43 +0700

    fix: rm coinbase localstorage if disconnect

--- src/hooks/useDisconnectWallet.ts
+++ src/hooks/useDisconnectWallet.ts
@@ -1,7 +1,7 @@
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useCallback } from 'react'
 
-import { SUPPORTED_WALLETS } from 'constants/wallets'
+import { SUPPORTED_WALLETS, WALLETLINK_LOCALSTORAGE_NAME } from 'constants/wallets'
 import { useActiveWeb3React, useWeb3React } from 'hooks'
 import { useIsUserManuallyDisconnect } from 'state/user/hooks'
 import { isEVMWallet, isSolanaWallet } from 'utils'
@@ -13,11 +13,15 @@ const useDisconnectWallet = () => {
   const { connector, deactivate } = useWeb3React()
   return useCallback(() => {
     const wallet = walletKey && SUPPORTED_WALLETS[walletKey]
+    const onRemove = () => {
+      setIsUserManuallyDisconnect(true)
+      localStorage.removeItem(WALLETLINK_LOCALSTORAGE_NAME)
+    }
     //If wallet support both network, disconnect to both
     if (wallet && isEVMWallet(wallet) && isSolanaWallet(wallet)) {
       deactivate()
       disconnect()
-      setIsUserManuallyDisconnect(true)
+      onRemove()
       return
     }
 
@@ -28,7 +32,7 @@ const useDisconnectWallet = () => {
     } else if (isSolana) {
       disconnect()
     }
-    setIsUserManuallyDisconnect(true)
+    onRemove()
   }, [connector, deactivate, disconnect, isEVM, isSolana, setIsUserManuallyDisconnect, walletKey])
 }
 export default useDisconnectWallet
