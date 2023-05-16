a4011223f4626be667ed98a50a899274f27e0061
Danh <nguyenhoaidanh2096@gmail.com>
2023-05-16 10:23:25 +0700

    wip

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -13,7 +13,12 @@ import { injected, trustWalletConnector, walletconnect, walletlink } from 'conne
 import { MOCK_ACCOUNT_EVM, MOCK_ACCOUNT_SOLANA } from 'constants/env'
 import { NETWORKS_INFO } from 'constants/networks'
 import { NetworkInfo } from 'constants/networks/type'
-import { LS_LAST_WALLETKEY, SUPPORTED_WALLET, SUPPORTED_WALLETS, WALLETLINK_LOCALSTORAGE_NAME } from 'constants/wallets'
+import {
+  LOCALSTORAGE_LAST_WALLETKEY,
+  SUPPORTED_WALLET,
+  SUPPORTED_WALLETS,
+  WALLETLINK_LOCALSTORAGE_NAME,
+} from 'constants/wallets'
 import { AppState } from 'state'
 import { useKyberSwapConfig } from 'state/application/hooks'
 import { useConnectedWallet } from 'state/authen/hooks'
@@ -137,7 +142,7 @@ export async function isAuthorized(getAccount = false): Promise<string | boolean
   // Check if previous connected to Coinbase Link
   if (
     (window.localStorage.getItem(WALLETLINK_LOCALSTORAGE_NAME) ||
-      localStorage.getItem(LS_LAST_WALLETKEY) === 'WALLET_CONNECT') &&
+      localStorage.getItem(LOCALSTORAGE_LAST_WALLETKEY) === 'WALLET_CONNECT') &&
     !getAccount
   ) {
     return true
@@ -182,7 +187,7 @@ export function useEagerConnect() {
           connector = walletlink
           throwError = false
         } else if ((authorized && !isManuallyDisconnect) || (isMobile && window.ethereum)) {
-          const lastWalletKey = localStorage.getItem(LS_LAST_WALLETKEY)
+          const lastWalletKey = localStorage.getItem(LOCALSTORAGE_LAST_WALLETKEY)
           const wallet = lastWalletKey && SUPPORTED_WALLETS[lastWalletKey]
           connector = wallet && isEVMWallet(wallet) ? wallet.connector : injected
         } else if (detectedWallet === 'TRUST_WALLET' && !isManuallyDisconnect) {
