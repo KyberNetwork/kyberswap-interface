291799c55dfb587ebb018941c42e97acf251ed23
Hung Doan <19758667+hungdoansy@users.noreply.github.com>
2023-05-18 11:26:14 +0700

    refactor: prevent users from dismissing token warning modal without confirming (#1920)
    * refactor: not allow users to dismiss token warning modal
    
    * fix cannot import 2nd token after confirming the 1st one

--- src/components/TokenWarningModal/index.tsx
+++ src/components/TokenWarningModal/index.tsx
@@ -3,19 +3,21 @@ import { Currency, Token } from '@kyberswap/ks-sdk-core'
 import Modal from 'components/Modal'
 import { ImportToken } from 'components/SearchModal/ImportToken'
 
+const noop = () => {
+  // empty
+}
+
 export default function TokenWarningModal({
   isOpen,
   tokens,
   onConfirm,
-  onDismiss,
 }: {
   isOpen: boolean
   tokens: Token[]
   onConfirm: (token: Currency[]) => void
-  onDismiss: () => void
 }) {
   return (
-    <Modal isOpen={isOpen} onDismiss={onDismiss} maxHeight={100}>
+    <Modal isOpen={isOpen} onDismiss={noop} maxHeight={100}>
       <ImportToken tokens={tokens} handleCurrencySelect={onConfirm} enterToImport={isOpen} />
     </Modal>
   )
