5f3585c55df96ecaf356960e1597a87e6dc67baa
Hien Le <hien.le@kyber.network>
2023-05-19 17:25:09 +0700

    Update to test

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    baseUrl: `https://kyberswap-interface-${process.env.PR_NUMBER}.pr.kyberengineering.io/`,
+    baseUrl: `https://kyberswap-interface-${parseInt(process.env.GITHUB_REF_NAME + '')}.pr.kyberengineering.io/`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
