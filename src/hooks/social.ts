b47598ee407e86db2b36eebbb1cee75824757a6c
Danh <nguyenhoaidanh2096@gmail.com>
2023-05-22 17:59:10 +0700

    update to func
    c

--- src/hooks/social.ts
+++ src/hooks/social.ts
@@ -1,23 +1,31 @@
 import axios from 'axios'
+import { useCallback } from 'react'
 import { useUploadImageMutation } from 'services/social'
 import { v4 as uuid } from 'uuid'
 
 import { BUCKET_NAME } from 'constants/env'
 
-export const useUploadImageToCloud = async (blob: Blob) => {
+export const useUploadImageToCloud = () => {
   const [uploadImage] = useUploadImageMutation()
 
-  const fileName = `${uuid()}.png`
-  const file = new File([blob], fileName, { type: 'image/png' })
-  const res = await uploadImage({
-    fileName,
-  }).unwrap()
+  return useCallback(
+    async (blob: Blob) => {
+      try {
+        const fileName = `${uuid()}.png`
+        const file = new File([blob], fileName, { type: 'image/png' })
+        const res = await uploadImage({
+          fileName,
+        }).unwrap()
 
-  const url = res?.data?.data?.signedURL
-  if (!url) throw new Error('Upload error')
-  await axios({ url, method: 'PUT', data: file })
+        const url = res?.data?.signedURL
 
-  const imageUrl = `https://storage.googleapis.com/${BUCKET_NAME}/${fileName}`
-
-  return imageUrl
+        if (!url) throw new Error('Upload error')
+        await axios({ url, method: 'PUT', data: file })
+        return `https://storage.googleapis.com/${BUCKET_NAME}/${fileName}`
+      } catch (error) {
+        return
+      }
+    },
+    [uploadImage],
+  )
 }
