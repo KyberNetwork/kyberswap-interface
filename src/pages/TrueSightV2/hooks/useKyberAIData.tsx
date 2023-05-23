96010b3ae9a9279ef664d8f3658281840eb48b67
Danh <nguyenhoaidanh2096@gmail.com>
2023-05-23 11:05:38 +0700

    rm unuse

--- src/pages/TrueSightV2/hooks/useKyberAIData.tsx
+++ src/pages/TrueSightV2/hooks/useKyberAIData.tsx
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
+import { createApi } from '@reduxjs/toolkit/dist/query/react'
 import baseQueryOauth from 'services/baseQueryOauth'
 
 import { BFF_API } from 'constants/env'
