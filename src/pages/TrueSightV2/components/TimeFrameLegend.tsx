e2700c9ee2278384ac54443c1c06d4b1fa6549cf
XiaoYhun <xiaoyhun@gmail.com>
2023-05-29 13:56:25 +0700

    fix comment

--- src/pages/TrueSightV2/components/TimeFrameLegend.tsx
+++ src/pages/TrueSightV2/components/TimeFrameLegend.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 import styled, { css } from 'styled-components'
 
 import { KyberAITimeframe } from '../types'
