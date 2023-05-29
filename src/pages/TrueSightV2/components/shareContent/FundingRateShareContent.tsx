214e8d29241880422845c5db11de2148547637e5
XiaoYhun <xiaoyhun@gmail.com>
2023-05-29 14:59:55 +0700

    fix some bugs

--- src/pages/TrueSightV2/components/shareContent/FundingRateShareContent.tsx
+++ src/pages/TrueSightV2/components/shareContent/FundingRateShareContent.tsx
@@ -1,24 +1,10 @@
-import styled from 'styled-components'
-
 import Column from 'components/Column'
 
 import { FundingRateTable } from '../table'
 
-const ChartImage = styled.div`
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 100%;
-  height: 100%;
-`
-export default function FundingRateShareContent({ dataUrl, mobileMode }: { dataUrl?: string; mobileMode?: boolean }) {
+export default function FundingRateShareContent({ mobileMode }: { mobileMode?: boolean }) {
   return (
     <Column style={{ justifyContent: 'center', width: '100%', height: '100%' }} gap="16px">
-      <ChartImage
-        style={{
-          backgroundImage: `url(${dataUrl})`,
-        }}
-      />
       <FundingRateTable mobileMode={mobileMode} />
     </Column>
   )
