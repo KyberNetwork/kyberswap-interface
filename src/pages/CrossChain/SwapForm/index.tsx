bcae4c01f1c57b62117cc18e8cf48bd60df27781
Danh <nguyenhoaidanh2096@gmail.com>
2023-06-01 10:38:07 +0700

    add prefer params

--- src/pages/CrossChain/SwapForm/index.tsx
+++ src/pages/CrossChain/SwapForm/index.tsx
@@ -89,6 +89,7 @@ export default function SwapForm() {
       slippage: slippageTolerance / 100,
       enableExpress: enableExpressExecution,
       quoteOnly: !account,
+      prefer: ['KYBERSWAP_AGGREGATOR'],
     }
   }, [
     currencyIn,
