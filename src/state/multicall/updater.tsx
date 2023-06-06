51c1383a86da6a9e49f665d92e95bd15d18e55a8
Doan Sy Hung <hungdoansy@gmail.com>
2023-06-06 10:51:18 +0700

    hotfix: remove block check

--- src/state/multicall/updater.tsx
+++ src/state/multicall/updater.tsx
@@ -76,10 +76,10 @@ export async function fetchChunk(
     console.debug('Failed to fetch chunk inside retry', error)
     throw error
   }
-  if (resultsBlockNumber.toNumber() < minBlockNumber) {
-    console.debug(`Fetched results for old block number: ${resultsBlockNumber.toString()} vs. ${minBlockNumber}`)
-    throw new RetryableError('Fetched for old block number')
-  }
+  // if (resultsBlockNumber.toNumber() < minBlockNumber) {
+  //   console.debug(`Fetched results for old block number: ${resultsBlockNumber.toString()} vs. ${minBlockNumber}`)
+  //   throw new RetryableError('Fetched for old block number')
+  // }
   return { results: returnData, blockNumber: resultsBlockNumber.toNumber() }
 }
 
