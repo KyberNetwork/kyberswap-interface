4d4a718042cdcf3df81d3159ff6743fb2392c7de
Hien Le <hien.le@kyber.network>
2023-06-13 13:11:03 +0700

    Update e2e files

--- cypress/e2e/specs/swap-page.e2e-spec.cy.ts
+++ cypress/e2e/specs/swap-page.e2e-spec.cy.ts
@@ -30,7 +30,7 @@ describe('Token Catalog', () => {
       cy.clickButton(token.tokenIn)
       cy.selectTokenInFavoriteTokensList(token.favoriteToken, tokenSymbol[1])
       cy.verifySelectedToken(token.tokenIn, tokenSymbol[1])
-      cy.verifyURL(tokenSymbol[1], 'ABCs')
+      cy.verifyURL(tokenSymbol[1], '')
     })
 
     it('Should be selected tokenOut in favorite tokens list successfully', { tags: 'regression' }, () => {
