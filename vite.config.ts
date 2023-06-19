cb3fa50616e11ffc337aedea88011f7a4f6b6436
Nam Nguyen <nam.nguyen@kyber.network>
2023-06-19 22:09:45 +0700

    disable minify build

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   build: {
     outDir: 'build',
+    minify: false, // todo namgold: revert
   },
   plugins: [
     react({
