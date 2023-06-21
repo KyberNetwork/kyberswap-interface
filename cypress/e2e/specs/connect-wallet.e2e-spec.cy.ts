70a0bd62beab4180c60bbcff6aed42aaff827ba5
Hien Le <hien.le@kyber.network>
2023-06-21 17:59:43 +0700

    Update tests

--- cypress/e2e/specs/connect-wallet.e2e-spec.cy.ts
+++ cypress/e2e/specs/connect-wallet.e2e-spec.cy.ts
@@ -2,7 +2,7 @@ import { tag } from '../pages/swap-page.po.cy'
 import { homePage, network, wallet } from '../selectors/selectors.cy'
 
 const chainList = [
-  'BNB Chain',
+  'BNB',
   'Polygon',
   'Avalanche',
   'Arbitrum',
@@ -25,18 +25,19 @@ describe('Metamask Extension tests', () => {
 
   it('Redirects to swap page when a user has already connected a wallet', { tags: tag.regression }, () => {
     cy.acceptMetamaskAccess()
-    cy.get(wallet.statusConnected, { timeout: 5000 }).should('be.visible')
+    cy.get(wallet.statusConnected, { timeout: 10000 }).should('be.visible')
     cy.url().should('include', '/swap')
   })
 
   it('Should approve permission to switch network', { tags: tag.regression }, () => {
-    cy.get(wallet.statusConnected, { timeout: 5000 }).should('be.visible')
+    cy.get(wallet.statusConnected, { timeout: 10000 }).should('be.visible')
     chainList.forEach(element => {
       cy.clickButton(network.btnSelectNetwork)
       cy.get(network.btnNetwork).contains(element).click({ force: true })
       cy.allowMetamaskToAddAndSwitchNetwork().then(approved => {
         expect(approved).to.be.true
       })
+      cy.url().should('include', '/swap/' + element.toLowerCase())
     })
   })
 })
