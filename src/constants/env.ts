42fa7310b4d8eef12f0e7becd1934b0ee0ecd82f
Danh <nguyenhoaidanh2096@gmail.com>
2023-06-28 11:31:12 +0700

    firebase dev

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -61,6 +61,7 @@ type FirebaseConfig = {
   messagingSenderId: string
   databaseURL?: string
   appId: string
+  measurementId?: string
 }
 
 export const FIREBASE: { [key: string]: { DEFAULT: FirebaseConfig; LIMIT_ORDER?: FirebaseConfig } } = {
@@ -74,13 +75,13 @@ export const FIREBASE: { [key: string]: { DEFAULT: FirebaseConfig; LIMIT_ORDER?:
       appId: '1:522790089501:web:524403003ae65c09c727f4',
     },
     DEFAULT: {
-      apiKey: 'AIzaSyDszHtJ4CJq0mwjBJ1pTt5OOzG5tiooEsg',
-      authDomain: 'test-bace2.firebaseapp.com',
-      databaseURL: 'https://test-bace2-default-rtdb.asia-southeast1.firebasedatabase.app',
-      projectId: 'test-bace2',
-      storageBucket: 'test-bace2.appspot.com',
-      messagingSenderId: '337703820408',
-      appId: '1:337703820408:web:2fb16ef71941817dec618d',
+      apiKey: 'AIzaSyD1UhS2D-a5D6e20SuKgx87FGitbQUL_V8',
+      authDomain: 'notification-local-73771.firebaseapp.com',
+      projectId: 'notification-local-73771',
+      storageBucket: 'notification-local-73771.appspot.com',
+      messagingSenderId: '227120017035',
+      appId: '1:227120017035:web:36592ab3e81b5614b7ae8e',
+      measurementId: 'G-Q3EPQSWMM5',
     },
   },
   staging: {
