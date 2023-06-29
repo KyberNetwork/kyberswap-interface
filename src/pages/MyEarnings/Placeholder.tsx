297d7f14ece35bf6b15eeb8f45480c33c045bba9
Doan Sy Hung <hungdoansy@gmail.com>
2023-06-29 21:27:57 +0700

    update placeholder

--- src/pages/MyEarnings/Placeholder.tsx
+++ src/pages/MyEarnings/Placeholder.tsx
@@ -1,12 +1,43 @@
 import { Trans } from '@lingui/macro'
+import { useMedia } from 'react-use'
 import { Flex, Text } from 'rebass'
+import styled from 'styled-components'
 
-import PlaceholderImage from 'assets/images/my-earnings-placeholder.png'
+import DesktopDarkPlaceholder from 'assets/images/my_earnings_placeholder_desktop_dark.png'
+import DesktopLightPlaceholder from 'assets/images/my_earnings_placeholder_desktop_light.png'
+import MobileDarkPlaceholder from 'assets/images/my_earnings_placeholder_mobile_dark.png'
+import MobileLightPlaceholder from 'assets/images/my_earnings_placeholder_mobile_light.png'
 import { ButtonPrimary } from 'components/Button'
 import { useWalletModalToggle } from 'state/application/hooks'
+import { useDarkModeManager } from 'state/user/hooks'
+import { MEDIA_WIDTHS } from 'theme'
+
+const Image = styled.img`
+  width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+`
 
 const Placeholder = () => {
   const toggleWalletModal = useWalletModalToggle()
+  const [darkMode] = useDarkModeManager()
+  const upToSmall = useMedia(`(max-width: ${MEDIA_WIDTHS.upToSmall}px)`)
+
+  const getBackround = () => {
+    if (upToSmall) {
+      if (darkMode) {
+        return MobileDarkPlaceholder
+      }
+
+      return MobileLightPlaceholder
+    }
+
+    if (darkMode) {
+      return DesktopDarkPlaceholder
+    }
+
+    return DesktopLightPlaceholder
+  }
 
   return (
     <Flex
@@ -15,9 +46,9 @@ const Placeholder = () => {
         width: '100%',
         height: '100%',
         position: 'relative',
-        background: `url(${PlaceholderImage})`,
       }}
     >
+      <Image src={getBackround()} />
       <Flex
         sx={{
           flexDirection: 'column',
