c05af5e1891a3425486390fe7e863f44a6e43ad9
Doan Sy Hung <hungdoansy@gmail.com>
2023-07-06 10:24:46 +0700

    usd value format

--- src/components/EarningAreaChart/utils.ts
+++ src/components/EarningAreaChart/utils.ts
@@ -3,8 +3,8 @@ export const formatUSDValue = (v: number, compact = true): string => {
     return '$0'
   }
 
-  if (v < 0.0001) {
-    return '< $0.0001'
+  if (v < 0.01) {
+    return '< $0.01'
   }
 
   const formatter = Intl.NumberFormat('en-US', {
@@ -12,7 +12,7 @@ export const formatUSDValue = (v: number, compact = true): string => {
     currency: 'USD',
     notation: compact ? 'compact' : 'standard',
     minimumFractionDigits: 0,
-    maximumFractionDigits: 4,
+    maximumFractionDigits: 2,
   })
 
   return formatter.format(v)
