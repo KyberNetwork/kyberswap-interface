86837c66f00296830e29ea43d2a65d0091fd6f2f
Nam Nguyen <nam.nguyen@kyber.network>
2023-07-14 15:46:05 +0700

    add gas refund

--- src/components/SwapForm/SwapModal/SwapDetails/index.tsx
+++ src/components/SwapForm/SwapModal/SwapDetails/index.tsx
@@ -3,10 +3,12 @@ import { Trans } from '@lingui/macro'
 import { rgba } from 'polished'
 import { useState } from 'react'
 import { ExternalLink as ExternalLinkIcon, Repeat } from 'react-feather'
+import { NavLink } from 'react-router-dom'
 import { Flex, Text } from 'rebass'
 import { BuildRouteData } from 'services/route/types/buildRoute'
 
 import { TruncatedText } from 'components'
+import { ButtonLight } from 'components/Button'
 import { AutoColumn } from 'components/Column'
 import CopyHelper from 'components/Copy'
 import Divider from 'components/Divider'
@@ -17,8 +19,9 @@ import { TooltipTextOfSwapFee } from 'components/SwapForm/TradeSummary'
 import useCheckStablePairSwap from 'components/SwapForm/hooks/useCheckStablePairSwap'
 import { MouseoverTooltip, TextDashed } from 'components/Tooltip'
 import { StyledBalanceMaxMini } from 'components/swapv2/styleds'
-import { CHAINS_SUPPORT_FEE_CONFIGS } from 'constants/index'
+import { APP_PATHS, CHAINS_SUPPORT_FEE_CONFIGS } from 'constants/index'
 import { useActiveWeb3React } from 'hooks'
+import { useGasRefundTier } from 'hooks/kyberdao'
 import useTheme from 'hooks/useTheme'
 import { useIsDarkMode } from 'state/user/hooks'
 import { ExternalLink, TYPE } from 'theme'
@@ -71,11 +74,12 @@ export default function SwapDetails({
   priceImpact,
   buildData,
 }: Props) {
-  const { isEVM, chainId, networkInfo } = useActiveWeb3React()
+  const { isEVM, chainId, networkInfo, account } = useActiveWeb3React()
   const [showInverted, setShowInverted] = useState<boolean>(false)
   const theme = useTheme()
   const isDarkMode = useIsDarkMode()
   const { slippage, routeSummary } = useSwapFormContext()
+  const { gasRefundPerCentage } = useGasRefundTier()
 
   const currencyIn = routeSummary?.parsedAmountIn?.currency
   const currencyOut = routeSummary?.parsedAmountOut?.currency
@@ -323,6 +327,34 @@ export default function SwapDetails({
           <TYPE.black fontSize={12} color={checkWarningSlippage(slippage, isStablePair) ? theme.warning : undefined}>
             {formatSlippage(slippage)}
           </TYPE.black>
+        </RowBetween>
+
+        <RowBetween height="20px" style={{ gap: '16px' }}>
+          <RowFixed>
+            <TextDashed fontSize={12} fontWeight={400} color={theme.subText}>
+              <MouseoverTooltip
+                text={
+                  <Text>
+                    <Trans>
+                      Stake KNC in KyberDAO to get gas refund. Read more{' '}
+                      <ExternalLink href="https://docs.kyberswap.com/governance/knc-token/gas-refund-program">
+                        here ↗
+                      </ExternalLink>
+                    </Trans>
+                  </Text>
+                }
+                placement="right"
+              >
+                <Trans>Gas Refund</Trans>
+              </MouseoverTooltip>
+            </TextDashed>
+          </RowFixed>
+
+          <ExternalLink href={APP_PATHS.KYBERDAO_KNC_UTILITY}>
+            <ButtonLight padding="0px 8px" width="fit-content" fontSize={10} fontWeight={500} lineHeight="16px">
+              <Trans>{account ? gasRefundPerCentage * 100 : '--'}% Refund</Trans>
+            </ButtonLight>
+          </ExternalLink>
         </RowBetween>
 
         <Divider />
