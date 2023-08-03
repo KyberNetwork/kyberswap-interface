c9642c525ceee7fa1a7d2864d197087127ed9744
Nam Nguyen <nam.nguyen@kyber.network>
2023-08-03 08:45:26 +0700

    add more

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -29,6 +29,7 @@ export default class ErrorBoundary extends React.Component<
       error.name === 'ChunkLoadError' ||
       /Loading .*?chunk .*? failed/.test(error.message) ||
       error.message.includes('Importing a module script failed') ||
+      error.message.includes('error loading dynamically imported module') ||
       error.message.includes('Failed to fetch dynamically imported module') ||
       error.stack?.includes('Failed to fetch dynamically imported module')
     ) {
