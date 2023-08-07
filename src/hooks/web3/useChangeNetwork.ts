1e1a29180748be209a7c23bb5c00439325bcb8bb
viet-nv <vietnv1304@gmail.com>
2023-08-07 11:42:26 +0700

    fix: CR-537 KEP-1461

--- src/hooks/web3/useChangeNetwork.ts
+++ src/hooks/web3/useChangeNetwork.ts
@@ -141,7 +141,7 @@ export function useChangeNetwork() {
             method: 'wallet_addEthereumChain',
             params: [addChainParameter],
           })
-          changeNetworkHandler(desiredChainId, wrappedSuccessCallback)
+          wrappedSuccessCallback()
         } catch (error) {
           console.error('Add new network failed', { addChainParameter, error })
           failureCallback(connector, desiredChainId, error, customFailureCallback, customTexts)
@@ -160,7 +160,6 @@ export function useChangeNetwork() {
     [
       library?.provider,
       chainId,
-      changeNetworkHandler,
       connector,
       failureCallback,
       fetchKyberswapConfig,
@@ -212,7 +211,7 @@ export function useChangeNetwork() {
             desiredChainId,
             undefined,
             undefined,
-            customSuccessCallback,
+            () => changeNetwork(desiredChainId, customSuccessCallback, customFailureCallback, waitUtilUpdatedChainId),
             customFailureCallback,
             waitUtilUpdatedChainId,
           )
