253aa2424129c2270510bb8c8a219ee3de9c62e7
Danh <nguyenhoaidanh2096@gmail.com>
2023-08-29 15:00:39 +0700

    update styled

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,6 +1,7 @@
+import { forwardRef } from 'react'
 import styled, { CSSProperties, css } from 'styled-components'
 
-const StyledMobileTabButton = styled.div<{ active?: boolean }>`
+const StyledMobileTabButton = styled.div<{ active?: boolean; separator?: boolean }>`
   font-size: 12px;
   line-height: 16px;
   transition: all 0.2s ease;
@@ -11,6 +12,20 @@ const StyledMobileTabButton = styled.div<{ active?: boolean }>`
   flex: 1;
   box-sizing: border-box;
   cursor: pointer;
+  user-select: none;
+  ${({ theme, separator, active }) =>
+    separator &&
+    !active &&
+    css`
+      position: relative;
+      &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        height: 16px;
+        border: 1px solid ${theme.border};
+      }
+    `}
   ${({ theme, active }) =>
     active
       ? css`
@@ -28,20 +43,16 @@ const StyledMobileTabButton = styled.div<{ active?: boolean }>`
   }
 `
 
-export const TabButton = ({
-  text,
-  active,
-  onClick,
-  style,
-}: {
-  text?: string
-  active?: boolean
-  onClick?: () => void
-  style?: CSSProperties
-}) => {
+type Props = { text?: string; active?: boolean; onClick?: () => void; style?: CSSProperties; separator?: boolean }
+const TabButton = forwardRef<HTMLDivElement, Props>(function TabButton(
+  { text, active, onClick, style, separator },
+  ref,
+) {
   return (
-    <StyledMobileTabButton active={active} onClick={onClick} style={style}>
+    <StyledMobileTabButton active={active} onClick={onClick} style={style} separator={separator} ref={ref}>
       {text}
     </StyledMobileTabButton>
   )
-}
+})
+
+export default TabButton
