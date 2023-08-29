514b22b8e74bb7bdebb5f2a4c70f26eb9e74040a
Danh <nguyenhoaidanh2096@gmail.com>
2023-08-29 13:46:48 +0700

    split tab btn

--- src/pages/TrueSightV2/components/index.tsx
+++ src/pages/TrueSightV2/components/index.tsx
@@ -1,5 +1,5 @@
 import { Trans } from '@lingui/macro'
-import React, { CSSProperties, ReactNode, useLayoutEffect, useRef, useState } from 'react'
+import React, { ReactNode, useLayoutEffect, useRef, useState } from 'react'
 import { isMobile } from 'react-device-detect'
 import { useParams } from 'react-router'
 import { useMedia } from 'react-use'
@@ -10,6 +10,7 @@ import Column from 'components/Column'
 import Icon from 'components/Icons/Icon'
 import Modal from 'components/Modal'
 import Row, { RowBetween, RowFit } from 'components/Row'
+import { TabButton } from 'components/TabButton'
 import { MouseoverTooltip } from 'components/Tooltip'
 import { MIXPANEL_TYPE, useMixpanelKyberAI } from 'hooks/useMixpanel'
 import useTheme from 'hooks/useTheme'
@@ -417,61 +418,5 @@ export const SectionWrapper = ({
         />
       )}
     </StyledSectionWrapper>
-  )
-}
-
-const StyledMobileTabButton = styled.div<{ active?: boolean }>`
-  font-size: 12px;
-  line-height: 16px;
-  transition: all 0.2s ease;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 40px;
-  flex: 1;
-  box-sizing: border-box;
-  cursor: pointer;
-  ${({ theme, active }) =>
-    active
-      ? css`
-          color: ${theme.primary};
-          background-color: ${theme.primary + '40'};
-          box-shadow: inset 0 -2px 0 0 ${theme.primary};
-        `
-      : css`
-          color: ${theme.subText};
-          background-color: ${theme.background};
-        `}
-
-  :hover {
-    filter: brightness(1.2);
-  }
-`
-
-export const TabButton = ({
-  text,
-  active,
-  onClick,
-  style,
-}: {
-  text?: string
-  active?: boolean
-  onClick?: () => void
-  style?: CSSProperties
-}) => {
-  const above768 = useMedia(`(min-width: ${MEDIA_WIDTHS.upToSmall}px)`)
-
-  return (
-    <>
-      {above768 ? (
-        <StyledMobileTabButton active={active} onClick={onClick} style={style}>
-          {text}
-        </StyledMobileTabButton>
-      ) : (
-        <StyledMobileTabButton active={active} onClick={onClick} style={style}>
-          {text}
-        </StyledMobileTabButton>
-      )}
-    </>
   )
 }
