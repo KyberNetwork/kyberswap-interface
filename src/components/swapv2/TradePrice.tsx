b51b9f46801b0718ace0e716023bf4494a6210bd
Nam Nguyen <nam.nguyen@kyber.network>
2023-09-08 14:25:06 +0700

    increase precision

--- src/components/swapv2/TradePrice.tsx
+++ src/components/swapv2/TradePrice.tsx
@@ -24,7 +24,7 @@ export default function TradePrice({ price, label, icon, style = {}, color }: Tr
   const [showInverted, setShowInverted] = useState<boolean>(false)
   let formattedPrice
   try {
-    formattedPrice = formatDisplayNumber({ value: showInverted ? price?.invert() : price, significantDigits: 6 })
+    formattedPrice = formatDisplayNumber({ value: showInverted ? price?.invert() : price, significantDigits: 7 })
   } catch (error) {}
 
   const show = Boolean(price?.baseCurrency && price?.quoteCurrency && formattedPrice)
