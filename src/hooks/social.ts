bc0b57723d3ea010583782ab2a96491beb7ae9e2
Danh <nguyenhoaidanh2096@gmail.com>
2023-09-11 13:41:52 +0700

    required identity filename

--- src/hooks/social.ts
+++ src/hooks/social.ts
@@ -4,19 +4,22 @@ import { useUploadImageMutation } from 'services/social'
 import { v4 as uuid } from 'uuid'
 
 import { BUCKET_NAME } from 'constants/env'
+import { useSessionInfo } from 'state/authen/hooks'
 
 export const IMAGE_ALLOW_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
 
 export const useUploadImageToCloud = () => {
   const [uploadImage] = useUploadImageMutation()
+  const { signedUserInfo } = useSessionInfo()
 
   return useCallback(
     async (fileObject: Blob | File) => {
       try {
+        if (!signedUserInfo?.identityId) throw new Error('invalid operation')
         const file = fileObject as File
         const ext = file.name?.split('.')?.pop() ?? ''
         if (ext && !IMAGE_ALLOW_EXTENSIONS.includes(ext.toLowerCase())) throw new Error('File is not support')
-        const fileName = `${uuid() + Date.now()}.${ext || 'png'}`
+        const fileName = `${signedUserInfo?.identityId}_${uuid()}_${Date.now()}.${ext || 'png'}`
         const res = await uploadImage({
           fileName,
         }).unwrap()
@@ -37,6 +40,6 @@ export const useUploadImageToCloud = () => {
         return
       }
     },
-    [uploadImage],
+    [uploadImage, signedUserInfo?.identityId],
   )
 }
