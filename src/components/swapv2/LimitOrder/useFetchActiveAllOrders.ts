6ba7beb0a677992900d782780b4a735c87a5f939
Danh <nguyenhoaidanh2096@gmail.com>
2023-09-12 13:25:32 +0700

    fix: use fetch all

--- src/components/swapv2/LimitOrder/useFetchActiveAllOrders.ts
+++ src/components/swapv2/LimitOrder/useFetchActiveAllOrders.ts
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { useGetLOConfigQuery, useLazyGetListOrdersQuery } from 'services/limitOrder'
+import { useCallback, useMemo } from 'react'
+import { useGetLOConfigQuery, useGetListOrdersQuery } from 'services/limitOrder'
 
 import { LimitOrder, LimitOrderStatus } from 'components/swapv2/LimitOrder/type'
 import { useActiveWeb3React } from 'hooks'
@@ -18,45 +18,19 @@ export const useIsSupportSoftCancelOrder = () => {
 
 export default function useFetchActiveAllOrders(disabled = false) {
   const { account, chainId } = useActiveWeb3React()
-  const [getOrders] = useLazyGetListOrdersQuery()
-  const [orders, setOrders] = useState<LimitOrder[]>([])
+  const { data } = useGetListOrdersQuery(
+    { chainId, maker: account, status: LimitOrderStatus.ACTIVE, pageSize: 100 },
+    { skip: !account || disabled },
+  )
+
   const isSupportSoftCancel = useIsSupportSoftCancelOrder()
-
-  const getAllOrders = useCallback(async () => {
-    if (!account) return []
-
-    let orders: LimitOrder[] = []
-    const pageSize = 100
-    const maximumPage = 5
-    let page = 1
-    while (true) {
-      const { data } = await getOrders({
-        chainId,
-        maker: account,
-        status: LimitOrderStatus.ACTIVE,
-        pageSize,
-        page,
-      })
-      page++
-      const response = data?.orders ?? []
-      orders = orders.concat(response)
-      if (response.length < pageSize || page >= maximumPage) break // out of orders, and prevent infinity loop
-    }
-
-    return orders
-  }, [getOrders, account, chainId])
-
-  useEffect(() => {
-    if (disabled) return
-    getAllOrders().then(data => setOrders(data))
-  }, [getAllOrders, disabled])
-
   return useMemo(() => {
+    const orders = data?.orders || []
     const ordersSoftCancel = orders.filter(isSupportSoftCancel)
     return {
       orders,
       ordersSoftCancel,
       supportCancelGasless: ordersSoftCancel.length > 0,
     }
-  }, [orders, isSupportSoftCancel])
+  }, [data, isSupportSoftCancel])
 }
