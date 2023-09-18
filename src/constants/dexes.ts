2bc876b172ff6d3d44bc80c8334b47c29fa43fbc
Danh <nguyenhoaidanh2096@gmail.com>
2023-09-18 09:44:21 +0700

    enable lo v2

--- src/constants/dexes.ts
+++ src/constants/dexes.ts
@@ -5,6 +5,7 @@ export const KYBERSWAP_KS_DEXES_TO_UI_DEXES: { [key: string]: string | undefined
   kyberswap: 'kyberswapv1', // kyberswap classic old contract
   'kyberswap-static': 'kyberswapv1', // kyberswap classic new contract -> with static fee
   'kyberswap-limit-order': 'kyberswap-limit-order',
+  'kyberswap-limit-order-v2': 'kyberswap-limit-order-v2',
 }
 
 export const KYBERSWAP_UI_DEXES: {
@@ -27,6 +28,11 @@ export const KYBERSWAP_UI_DEXES: {
   'kyberswap-limit-order': {
     name: 'KyberSwap Limit Order',
     id: 'kyberswap-limit-order',
+    logoURL: 'https://kyberswap.com/favicon.ico',
+  },
+  'kyberswap-limit-order-v2': {
+    name: 'KyberSwap Limit Order V2',
+    id: 'kyberswap-limit-order-v2',
     logoURL: 'https://kyberswap.com/favicon.ico',
   },
 }
