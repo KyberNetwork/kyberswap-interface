945fe75dae954b6382b3c65c9f4ef43754fec9e1
Danh <nguyenhoaidanh2096@gmail.com>
2023-10-03 13:39:47 +0700

    update dropdown like desgin

--- src/pages/TrueSightV2/components/TokenFilter/WatchlistSelect.tsx
+++ src/pages/TrueSightV2/components/TokenFilter/WatchlistSelect.tsx
@@ -1,6 +1,6 @@
 import { Trans, t } from '@lingui/macro'
 import { useMemo, useState } from 'react'
-import { CSSProperties } from 'styled-components'
+import styled, { CSSProperties } from 'styled-components'
 
 import Icon from 'components/Icons/Icon'
 import Row from 'components/Row'
@@ -9,6 +9,13 @@ import { ActiveSelectItem, StyledSelect } from 'pages/TrueSightV2/components/Tok
 import { ManageListModal } from 'pages/TrueSightV2/components/WatchlistButton'
 import { useGetWatchlistInformationQuery } from 'pages/TrueSightV2/hooks/useKyberAIData'
 
+const Divider = styled.div`
+  height: 1px;
+  margin: 6px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.border};
+`
+
+const optionStyle: CSSProperties = { fontSize: '14px', padding: '10px 18px', alignItems: 'center' }
 const WatchlistSelect = ({
   menuStyle,
   onChange,
@@ -21,42 +28,41 @@ const WatchlistSelect = ({
   const [isOpen, setIsOpen] = useState(false)
   const { data: dataWatchList } = useGetWatchlistInformationQuery()
 
-  const watchlistesOptions = useMemo(() => {
-    let total = 0
+  const { options, totalToken } = useMemo(() => {
+    let totalToken = 0
     const opts: SelectOption[] =
       dataWatchList?.watchlists?.map(e => {
-        total += e.assetNumber
+        totalToken += e.assetNumber
         return { value: e.id + '', label: `${e.name} (${e.assetNumber})` }
       }) || []
-
-    opts.unshift({
-      label: (
-        <Row>
-          <Trans>All Tokens ({total})</Trans>
-        </Row>
-      ),
-      value: '',
-    })
-    opts.push({
-      label: (
-        <Row gap="6px">
-          <Icon id="assignment" size={20} /> <Trans>Manage Lists</Trans>
-        </Row>
-      ),
-      onSelect: () => setIsOpen(true),
-    }) // todo danh: update like desgin
-
-    return opts
+    return { options: opts, totalToken }
   }, [dataWatchList])
+
+  const labelAll = <Trans>All Tokens ({totalToken})</Trans>
 
   return (
     <>
       <StyledSelect
         value={value}
-        activeRender={item => <ActiveSelectItem name={t`Watchlist`} label={item?.label} />}
-        options={watchlistesOptions}
+        activeRender={item => <ActiveSelectItem name={t`Watchlist`} label={value ? item?.label : labelAll} />}
+        options={options}
         onChange={onChange}
-        optionStyle={{ fontSize: '14px' }}
+        dropdownRender={menu => {
+          return (
+            <>
+              <Row style={optionStyle} onClick={() => onChange('')}>
+                {labelAll}
+              </Row>
+              <Divider />
+              {menu}
+              <Divider />
+              <Row style={optionStyle} gap="6px" onClick={() => setIsOpen(true)}>
+                <Icon id="assignment" size={20} /> <Trans>Manage Lists</Trans>
+              </Row>
+            </>
+          )
+        }}
+        optionStyle={optionStyle}
         menuStyle={menuStyle}
       />
       <ManageListModal isOpen={isOpen} setIsOpen={setIsOpen} />
