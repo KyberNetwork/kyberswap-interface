c70b94ec9c209dce4400b11159f62c0c60d9e985
Danh <nguyenhoaidanh2096@gmail.com>
2023-10-05 18:44:38 +0700

    add option all chains

--- src/pages/MyEarnings/MultipleChainSelectV2/index.tsx
+++ src/pages/MyEarnings/MultipleChainSelectV2/index.tsx
@@ -1,16 +1,18 @@
 import { ChainId } from '@kyberswap/ks-sdk-core'
-import { ReactNode, useRef, useState } from 'react'
+import { Trans } from '@lingui/macro'
+import { ReactNode, useState } from 'react'
 import { Box, Flex, Text } from 'rebass'
 import styled, { CSSProperties } from 'styled-components'
 
+import { ReactComponent as LogoKyber } from 'assets/svg/logo_kyber.svg'
 import Checkbox from 'components/CheckBox'
 import Select from 'components/SelectV2'
 import { MouseoverTooltip } from 'components/Tooltip'
 import { NETWORKS_INFO } from 'constants/networks'
-import { useOnClickOutside } from 'hooks/useOnClickOutside'
+import useChainsConfig from 'hooks/useChainsConfig'
 import useTheme from 'hooks/useTheme'
 
-import { ApplyButton } from './PopoverBody'
+import { ApplyButton } from './ApplyButton'
 import SelectButton from './SelectButton'
 
 export const StyledLogo = styled.img`
@@ -32,24 +34,31 @@ export type MultipleChainSelectProps = {
   activeRender?: (node: ReactNode) => ReactNode
 }
 const MultipleChainSelect: React.FC<MultipleChainSelectProps> = ({ className, style, ...props }) => {
-  const [expanded, setExpanded] = useState(false)
-  const ref = useRef<HTMLDivElement>(null)
-
-  const collapse = () => {
-    setExpanded(false)
-  }
-
-  useOnClickOutside(ref, expanded ? collapse : undefined)
-  const { comingSoonList = [], selectedChainIds = [], handleChangeChains } = props
-  const options = props.chainIds.map(id => ({ value: id, label: id }))
+  const { comingSoonList = [], selectedChainIds = [], handleChangeChains, chainIds = [], onTracking } = props
+  const options = chainIds.map(id => ({ value: id, label: id }))
   const theme = useTheme()
   const selectedChains = selectedChainIds.filter(item => !comingSoonList.includes(item))
   const [localSelectedChains, setLocalSelectedChains] = useState(() => selectedChains)
+  const { activeChains } = useChainsConfig()
+  const networkList = chainIds.filter(
+    item => !comingSoonList.includes(item) && activeChains.some(e => e.chainId === item),
+  )
+  const isAllSelected = localSelectedChains.length === networkList.length
+
+  const onChangeChain = () => {
+    if (isAllSelected) {
+      setLocalSelectedChains([])
+    } else {
+      onTracking?.()
+      setLocalSelectedChains(networkList)
+    }
+  }
+
   return (
     <Select
       className={className}
       style={{ ...style, flex: '0 0 150px', width: '150px', position: 'relative', zIndex: '3' }}
-      activeRender={_ => <SelectButton onClick={() => setExpanded(e => !e)} {...props} />}
+      activeRender={_ => <SelectButton {...props} />}
       options={options}
       optionRender={item => {
         const network = Number(item?.value) as ChainId
@@ -105,6 +114,32 @@ const MultipleChainSelect: React.FC<MultipleChainSelectProps> = ({ className, st
       dropdownRender={menu => {
         return (
           <>
+            <Flex
+              onClick={e => e.stopPropagation()}
+              sx={{
+                alignItems: 'center',
+                gap: '8px',
+                padding: '10px 18px',
+              }}
+            >
+              <Checkbox type="checkbox" checked={isAllSelected} onChange={onChangeChain} />
+
+              <Flex width="20px" alignItems="center" justifyContent="center">
+                <LogoKyber width="14px" height="auto" color={theme.primary} />
+              </Flex>
+
+              <Text
+                as="span"
+                sx={{
+                  fontWeight: 500,
+                  fontSize: '14px',
+                  lineHeight: '20px',
+                  color: theme.text,
+                }}
+              >
+                <Trans>All Chains</Trans>
+              </Text>
+            </Flex>
             {menu}
             <Box sx={{ margin: '10px 0', width: '100%', height: '0', borderBottom: `1px solid ${theme.border}` }} />
             <Flex padding={'0 22px'}>
@@ -112,7 +147,6 @@ const MultipleChainSelect: React.FC<MultipleChainSelectProps> = ({ className, st
                 disabled={!localSelectedChains.length}
                 onClick={() => {
                   handleChangeChains(localSelectedChains)
-                  // onClose()
                 }}
                 numOfChains={localSelectedChains.length}
               />
