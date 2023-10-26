2d9e0460d19af96a1caa4104f541e4154380bce1
Nguyễn Hoài Danh <33005392+nguyenhoaidanh@users.noreply.github.com>
2023-10-26 11:42:50 +0700

    chore: add tracking LO get started btn

--- src/components/swapv2/LimitOrder/Tutorial.tsx
+++ src/components/swapv2/LimitOrder/Tutorial.tsx
@@ -10,6 +10,7 @@ import { ButtonPrimary } from 'components/Button'
 import Column from 'components/Column'
 import Row from 'components/Row'
 import { DOCS_LINKS } from 'components/swapv2/LimitOrder/const'
+import useMixpanel, { MIXPANEL_TYPE } from 'hooks/useMixpanel'
 import useTheme from 'hooks/useTheme'
 import { ExternalLink } from 'theme'
 
@@ -50,6 +51,12 @@ const ContentItem = ({ text }: { text: ReactNode }) => {
 }
 export default function Tutorial({ onClose }: { onClose: () => void }) {
   const theme = useTheme()
+  const { mixpanelHandler } = useMixpanel()
+
+  const onGetStart = () => {
+    mixpanelHandler(MIXPANEL_TYPE.LO_CLICK_GET_STARTED)
+    onClose()
+  }
 
   return (
     <Wrapper>
@@ -97,7 +104,7 @@ export default function Tutorial({ onClose }: { onClose: () => void }) {
             Learn more about our limit orders <ExternalLink href={DOCS_LINKS.USER_GUIDE}>here</ExternalLink>.
           </Trans>
         </Text>
-        <ButtonPrimary height="40px" width={'100%'} onClick={onClose}>
+        <ButtonPrimary height="40px" width={'100%'} onClick={onGetStart}>
           <Trans>Get Started</Trans>
         </ButtonPrimary>
       </Column>
