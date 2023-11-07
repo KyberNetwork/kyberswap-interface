d257e5a3deecd59f150f5b900c6a887c873f6955
Danh <nguyenhoaidanh2096@gmail.com>
2023-11-07 13:51:45 +0700

    css

--- src/components/swapv2/LimitOrder/LimitOrderForm.tsx
+++ src/components/swapv2/LimitOrder/LimitOrderForm.tsx
@@ -765,7 +765,7 @@ const LimitOrderForm = forwardRef<LimitOrderFormHandle, Props>(function LimitOrd
               {currencyIn && currencyOut && (
                 <Flex style={{ gap: 6, cursor: 'pointer' }} onClick={() => onInvertRate(!rateInfo.invert)}>
                   <CurrencyLogo size={'18px'} currency={rateInfo.invert ? currencyIn : currencyOut} />
-                  <Text fontSize={14} color={theme.subText}>
+                  <Text fontSize={14} color={theme.subText} sx={{ userSelect: 'none' }}>
                     {rateInfo.invert ? currencyIn?.symbol : currencyOut?.symbol}
                   </Text>
                   <div>
