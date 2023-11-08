211908b28e6d2214ac9eb62cdd9c2093023c3285
hadothanh <ha.do@kyber.network>
2023-11-08 19:32:07 +0700

    add more test on arbitrum

--- cypress/e2e/specs/zap.e2e.cy.ts
+++ cypress/e2e/specs/zap.e2e.cy.ts
@@ -4,7 +4,6 @@ import { Network, SwapPage } from '../pages/swap-page.po.cy'
 import { DEFAULT_NETWORK, DEFAULT_URL, NETWORK, TAG } from '../selectors/constants.cy'
 
 const wallet = new Network()
-
 describe('Zap In', { tags: TAG.zap }, () => {
   before(() => {
     SwapPage.open(DEFAULT_URL)
@@ -24,8 +23,14 @@ describe('Zap In', { tags: TAG.zap }, () => {
   it('Arbitrum: wstETH-axl.wstETH', function () {
     PoolsPage.addLiquidity('0x83fe9065ed68506a0d2ece59cd71c43bbff6e450', '100')
   })
+
   it('Arbitrum: ETH-ARB', function () {
     SwapPage.goToPoolPage()
     PoolsPage.addLiquidity('0xdf03ca6c633f784ac5e062dd708b15728b488621', '100')
+  })
+
+  it('Arbitrum: USDC-USDC.e', function () {
+    SwapPage.goToPoolPage()
+    PoolsPage.addLiquidity('0xc23f1d198477c0bcae0cac2ec734ceda438a8990', '100')
   })
 })
