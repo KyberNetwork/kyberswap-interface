c60b5924df2f7f00e5e6404224a13ff174cc52ac
Danh <nguyenhoaidanh2096@gmail.com>
2023-11-29 16:25:07 +0700

    update time

--- src/pages/NotificationCenter/Portfolio/const.ts
+++ src/pages/NotificationCenter/Portfolio/const.ts
@@ -1,3 +1,5 @@
+import { TIMES_IN_SECS } from 'constants/index'
+
 export const MAXIMUM_PORTFOLIO = 2
 
-export const PORTFOLIO_POLLING_INTERVAL = 30_000
+export const PORTFOLIO_POLLING_INTERVAL = 5 * TIMES_IN_SECS.ONE_MIN * 1000
