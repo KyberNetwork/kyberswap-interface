2c97c2a0d154ab665e566d28c0b35a2a5994b8f4
Danh <nguyenhoaidanh2096@gmail.com>
2023-12-05 15:00:19 +0700

    allow filter columns

--- src/pages/NotificationCenter/Portfolio/PortfolioDetail/Tokens/WalletInfo.tsx
+++ src/pages/NotificationCenter/Portfolio/PortfolioDetail/Tokens/WalletInfo.tsx
@@ -1,7 +1,7 @@
 import { ChainId } from '@kyberswap/ks-sdk-core'
 import { Trans, t } from '@lingui/macro'
 import { rgba } from 'polished'
-import { useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { isMobile } from 'react-device-detect'
 import { Flex, Text } from 'rebass'
 import { useGetWalletsAllocationQuery } from 'services/portfolio'
@@ -11,7 +11,6 @@ import styled, { CSSProperties } from 'styled-components'
 import { ButtonAction } from 'components/Button'
 import Column from 'components/Column'
 import Icon from 'components/Icons/Icon'
-import TransactionSettingsIcon from 'components/Icons/TransactionSettingsIcon'
 import Wallet from 'components/Icons/Wallet'
 import LocalLoader from 'components/LocalLoader'
 import { TokenLogoWithChain } from 'components/Logo'
@@ -20,6 +19,7 @@ import Table, { TableColumn } from 'components/Table'
 import { EMPTY_ARRAY } from 'constants/index'
 import useDebounce from 'hooks/useDebounce'
 import useTheme from 'hooks/useTheme'
+import ActionPopup from 'pages/NotificationCenter/Portfolio/PortfolioDetail/Tokens/ActionPopup'
 // import { LiquidityScore } from 'pages/NotificationCenter/Portfolio/PortfolioDetail/Tokens/TokenAllocation'
 import useFilterBalances from 'pages/NotificationCenter/Portfolio/PortfolioDetail/Tokens/useFilterBalances'
 import { PortfolioSection, SearchPortFolio } from 'pages/NotificationCenter/Portfolio/PortfolioDetail/styled'
@@ -104,11 +104,24 @@ export const TokenCellWithWalletAddress = ({
   )
 }
 
-const ActionTitle = () => {
-  const theme = useTheme()
+export type DisplayField = { title: string; show: boolean; key: string }
+const defaultFields = [
+  { title: t`Price`, show: true, key: 'priceUsd' },
+  { title: t`Balance`, show: true, key: 'amount' },
+  { title: t`Value USD`, show: true, key: 'valueUsd' },
+  { title: t`KyberScore`, show: true, key: 'kyberScore' },
+]
+
+const ActionTitle = ({
+  displayFields,
+  onChangeDisplayField,
+}: {
+  displayFields: DisplayField[]
+  onChangeDisplayField: (fields: DisplayField[]) => void
+}) => {
   return (
     <Flex alignItems={'center'} sx={{ gap: '4px' }} justifyContent={'flex-end'}>
-      Action <TransactionSettingsIcon fill={theme.subText} size={16} />
+      Action <ActionPopup fields={displayFields} onChangeDisplayField={onChangeDisplayField} />
     </Flex>
   )
 }
@@ -161,90 +174,105 @@ const KyberScore = ({
   )
 }
 
-const columns: TableColumn<PortfolioWalletBalance>[] = [
-  {
-    title: t`Token`,
-    dataIndex: 'token',
-    align: 'left',
-    render: ({ item: { tokenLogo, tokenSymbol, chainId, walletAddress, tokenAddress } }) => (
-      <TokenCellWithWalletAddress
-        item={{ logoUrl: tokenLogo, symbol: tokenSymbol, walletAddress, chainId, tokenAddress }}
-      />
-    ),
-    sticky: true,
-    style: isMobile ? { width: 140 } : undefined,
-  },
-  {
-    title: t`Balance`,
-    dataIndex: 'amount',
-    render: ({ value }) => formatDisplayNumber(value, { style: 'decimal', significantDigits: 6 }),
-    style: isMobile ? { width: 120 } : undefined,
-    align: 'left',
-  },
-  {
-    title: t`Price`,
-    dataIndex: 'priceUsd',
-    render: ({ value }) => formatDisplayNumber(value, { style: 'currency', significantDigits: 6 }),
-    align: 'left',
-    style: isMobile ? { width: 120 } : undefined,
-  },
-  {
-    title: t`Value`,
-    dataIndex: 'valueUsd',
-    render: ({ value }) => formatDisplayNumber(value, { style: 'currency', fractionDigits: 2 }),
-    style: isMobile ? { width: 120 } : undefined,
-    align: 'left',
-  },
-  // {
-  //   title: t`Liquidity Score`,
-  //   dataIndex: 'token',
-  //   render: LiquidityScore,
-  //   tooltip: (
-  //     <Trans>
-  //       Liquidity Score of a token refers to how easily that token can be bought or sold in the market without
-  //       significantly impacting its price. Read more <ExternalLink href="/todo">here ↗</ExternalLink>
-  //     </Trans>
-  //   ),
-  //   style: isMobile ? { width: 120 } : undefined,
-  // },
-  // {
-  //   title: t`24H Volatility Score`,
-  //   dataIndex: 'token',
-  //   render: () => 'test',
-  //   tooltip: (
-  //     <Trans>
-  //       Volatility score measures the price volatility of the token. Find out more about the score{' '}
-  //       <ExternalLink href="/todo">here ↗</ExternalLink>
-  //     </Trans>
-  //   ),
-  //   style: isMobile ? { width: 120 } : undefined,
-  // },
-  {
-    title: t`KyberScore`,
-    dataIndex: 'kyberScore',
-    render: KyberScore,
-    tooltip: (
-      <Trans>
-        KyberScore uses AI to measure the upcoming trend of a token (bullish or bearish) by taking into account multiple
-        on-chain and off-chain indicators. The score ranges from 0 to 100. The higher the score, the more bullish the
-        token in the short-term. Read more <ExternalLink href="/todo">here ↗</ExternalLink>
-      </Trans>
-    ),
-    style: isMobile ? { width: 120 } : undefined,
-  },
-  {
-    title: <ActionTitle />,
-    align: 'right',
-    dataIndex: 'token',
-    render: ActionButton,
-  },
-]
+const getColumns = (displayFields: DisplayField[], onChangeDisplayField: (fields: DisplayField[]) => void) => {
+  return [
+    {
+      title: t`Token`,
+      align: 'left',
+      render: ({
+        item: { tokenLogo, tokenSymbol, chainId, walletAddress, tokenAddress },
+      }: {
+        item: PortfolioWalletBalance
+      }) => (
+        <TokenCellWithWalletAddress
+          item={{ logoUrl: tokenLogo, symbol: tokenSymbol, walletAddress, chainId, tokenAddress }}
+        />
+      ),
+      sticky: true,
+      style: isMobile ? { width: 140 } : undefined,
+    },
+    {
+      title: t`Balance`,
+      dataIndex: 'amount',
+      render: ({ value }: { value: string }) => formatDisplayNumber(value, { style: 'decimal', significantDigits: 6 }),
+      style: isMobile ? { width: 120 } : undefined,
+      align: 'left',
+    },
+    {
+      title: t`Price`,
+      dataIndex: 'priceUsd',
+      render: ({ value }: { value: string }) => formatDisplayNumber(value, { style: 'currency', significantDigits: 6 }),
+      align: 'left',
+      style: isMobile ? { width: 120 } : undefined,
+    },
+    {
+      title: t`Value`,
+      dataIndex: 'valueUsd',
+      render: ({ value }: { value: string }) => formatDisplayNumber(value, { style: 'currency', fractionDigits: 2 }),
+      style: isMobile ? { width: 120 } : undefined,
+      align: 'left',
+    },
+    // {
+    //   title: t`Liquidity Score`,
+    //   dataIndex: 'token',
+    //   render: LiquidityScore,
+    //   tooltip: (
+    //     <Trans>
+    //       Liquidity Score of a token refers to how easily that token can be bought or sold in the market without
+    //       significantly impacting its price. Read more <ExternalLink href="/todo">here ↗</ExternalLink>
+    //     </Trans>
+    //   ),
+    //   style: isMobile ? { width: 120 } : undefined,
+    // },
+    // {
+    //   title: t`24H Volatility Score`,
+    //   dataIndex: 'token',
+    //   render: () => 'test',
+    //   tooltip: (
+    //     <Trans>
+    //       Volatility score measures the price volatility of the token. Find out more about the score{' '}
+    //       <ExternalLink href="/todo">here ↗</ExternalLink>
+    //     </Trans>
+    //   ),
+    //   style: isMobile ? { width: 120 } : undefined,
+    // },
+    {
+      title: t`KyberScore`,
+      dataIndex: 'kyberScore',
+      render: KyberScore,
+      tooltip: (
+        <Trans>
+          KyberScore uses AI to measure the upcoming trend of a token (bullish or bearish) by taking into account
+          multiple on-chain and off-chain indicators. The score ranges from 0 to 100. The higher the score, the more
+          bullish the token in the short-term. Read more <ExternalLink href="/todo">here ↗</ExternalLink>
+        </Trans>
+      ),
+      style: isMobile ? { width: 120 } : undefined,
+    },
+    {
+      title: <ActionTitle displayFields={displayFields} onChangeDisplayField={onChangeDisplayField} />,
+      align: 'right',
+      render: ActionButton,
+    },
+  ].filter(el =>
+    displayFields.some(field => (el.dataIndex ? field.key === el.dataIndex && field.show : true)),
+  ) as TableColumn<PortfolioWalletBalance>[]
+}
+
 export default function WalletInfo({ walletAddresses, chainIds }: { walletAddresses: string[]; chainIds: ChainId[] }) {
   const theme = useTheme()
   const { data, isFetching } = useGetWalletsAllocationQuery(
     { walletAddresses, chainIds },
     { skip: !walletAddresses.length, refetchOnMountOrArgChange: true, pollingInterval: PORTFOLIO_POLLING_INTERVAL },
   )
+
+  const [displayFields, setDisplayFields] = useState(defaultFields)
+  const onChangeDisplayField = useCallback((fields: DisplayField[]) => {
+    setDisplayFields(fields)
+  }, [])
+  const columns = useMemo(() => {
+    return getColumns(displayFields, onChangeDisplayField)
+  }, [displayFields, onChangeDisplayField])
 
   const [search, setSearch] = useState('')
   const searchDebounce = useDebounce(search, 500)
