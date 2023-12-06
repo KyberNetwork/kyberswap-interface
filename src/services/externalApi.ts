fed631a6bd0eeb1886f7505beeb00c11a63f190a
WindzLord <ii.khackien@gmail.com>
2023-12-06 15:50:13 +0700

    Make useGasPrice from debank
    Minor

--- src/services/externalApi.ts
+++ src/services/externalApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 import { IPhaseData } from 'hooks/useClaimReward'
+import { GasPriceData } from 'hooks/useGasPriceFromDeBank'
 
 const externalApi = createApi({
   reducerPath: 'externalApi',
@@ -9,6 +10,12 @@ const externalApi = createApi({
     getClaimRewards: builder.query<IPhaseData[], { url: string; account?: string }>({
       query: ({ url }) => ({
         url,
+      }),
+    }),
+    getGasPrice: builder.query<GasPriceData, { chainSlug: string }>({
+      query: ({ chainSlug }) => ({
+        url: 'https://openapi.debank.com/v1/wallet/gas_market',
+        params: { chain_id: chainSlug },
       }),
     }),
   }),
