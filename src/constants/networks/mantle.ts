b5f2b0f0eae9459749b9b9c6ddffb34d95155036
viet-nv <vietnv1304@gmail.com>
2024-03-21 15:52:46 +0700

    change mantle rpc

--- src/constants/networks/mantle.ts
+++ src/constants/networks/mantle.ts
@@ -27,7 +27,7 @@ const mantle: NetworkInfo = {
     decimal: 18,
     minForGas: 2 * 10 ** 17,
   },
-  defaultRpcUrl: 'https://rpc.mantle.xyz',
+  defaultRpcUrl: 'https://rpc.ankr.com/mantle',
   multicall: '0xcA11bde05977b3631167028862bE2a173976CA11',
   classic: {
     defaultSubgraph: '',
