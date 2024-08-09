d7cefcc69b8f880d90e160933a987e957435b82f
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2024-08-09 15:20:18 +0700

    fix: add groupBy to object type

--- src/types/object.d.ts
+++ src/types/object.d.ts
@@ -4,4 +4,23 @@ interface ObjectConstructor {
    * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
    */
   keys<T extends string>(o: { [keys in T]: unknown }): T[]
+
+  /**
+   * Groups members of an iterable according to the return value of the passed callback.
+   * @param items An iterable.
+   * @param keySelector A callback which will be invoked for each item in items.
+   */
+  groupBy<K extends PropertyKey, T>(
+    items: Iterable<T>,
+    keySelector: (item: T, index: number) => K,
+  ): Partial<Record<K, T[]>>
+}
+
+interface MapConstructor {
+  /**
+   * Groups members of an iterable according to the return value of the passed callback.
+   * @param items An iterable.
+   * @param keySelector A callback which will be invoked for each item in items.
+   */
+  groupBy<K, T>(items: Iterable<T>, keySelector: (item: T, index: number) => K): Map<K, T[]>
 }
