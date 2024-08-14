fab50f9cfee3952ba64043540176997f245f6553
Tien Nguyen <tiennguyen@Nguyens-MacBook-Pro-4.local>
2024-08-14 14:34:41 +0700

    fix inline-style for TabButton

--- src/components/swapv2/LimitOrder/ListLimitOrder/TabSelector.tsx
+++ src/components/swapv2/LimitOrder/ListLimitOrder/TabSelector.tsx
@@ -1,10 +1,7 @@
 import { t } from '@lingui/macro'
-import { useMemo } from 'react'
-import { useMedia } from 'react-use'
 import styled from 'styled-components'
 
 import TabButton from 'components/TabButton'
-import { MEDIA_WIDTHS } from 'theme'
 
 import { LimitOrderTab } from '../type'
 
@@ -20,6 +17,17 @@ const TabSelectorWrapper = styled.div`
   `};
 `
 
+const StyledTabButton = styled(TabButton)`
+  padding: 16px;
+  flex: unset;
+  font-size: 14px;
+  width: fit-content;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 50%;
+  `};
+`
+
 export default function TabSelector({
   activeTab,
   setActiveTab,
@@ -27,22 +35,14 @@ export default function TabSelector({
   activeTab: LimitOrderTab
   setActiveTab: (n: LimitOrderTab) => void
 }) {
-  const upToSmall = useMedia(`(max-width: ${MEDIA_WIDTHS.upToSmall}px)`)
-  const style = useMemo(
-    () => ({ padding: '16px', flex: 'unset', fontSize: '14px', width: upToSmall ? '50%' : 'fit-content' }),
-    [upToSmall],
-  )
-
   return (
     <TabSelectorWrapper>
-      <TabButton
-        style={style}
+      <StyledTabButton
         active={activeTab === LimitOrderTab.ORDER_BOOK}
         onClick={() => setActiveTab(LimitOrderTab.ORDER_BOOK)}
         text={t`Open Limit Orders`}
       />
-      <TabButton
-        style={style}
+      <StyledTabButton
         active={activeTab === LimitOrderTab.MY_ORDER}
         text={t`My Order(s)`}
         onClick={() => setActiveTab(LimitOrderTab.MY_ORDER)}
