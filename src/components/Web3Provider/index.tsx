7512377b647da5208e957668dfeaf02cd4fb9337
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2024-10-16 10:47:00 +0700

    Test custom safe app with options

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -174,7 +174,10 @@ export const wagmiConfig = createConfig({
       enableMobileWalletLink: true,
     }),
     blocto({ appId: 'fbd356ae-8f39-4650-ab42-4ef8cb9e15c9' }),
-    safe(),
+    safe({
+      allowedDomains: [/^app\.safe\.global$/],
+      debug: false,
+    }),
   ],
   client({ chain }) {
     return createClient({
