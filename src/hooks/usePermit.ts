3752011aebc7059acb55c9c05f9a2aa8f1ad4169
viet-nv <vietnv1304@gmail.com>
2024-12-04 16:11:31 +0700

    fix: use version from api

--- src/hooks/usePermit.ts
+++ src/hooks/usePermit.ts
@@ -139,17 +139,18 @@ export const usePermit = (currencyAmount?: CurrencyAmount<Currency>, routerAddre
               name: currency.name,
               verifyingContract: currency.address,
               salt: hexZeroPad(hexlify(chainId), 32),
-              version: '1',
+              version: overwritedPermitData.version,
             }
           : {
               name: currency.name,
               verifyingContract: currency.address,
-              version: '1',
+              version: overwritedPermitData.version,
               chainId,
             },
       primaryType: 'Permit',
       message: message,
     })
+
     try {
       const signature = await library
         .send('eth_signTypedData_v4', [account.toLowerCase(), data])
