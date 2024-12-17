6541ca34d684ea2d58734c4b0560f35063b2c84a
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2024-12-17 16:24:46 +0700

    update new widget

--- src/pages/Earns/useLiquidityWidget.tsx
+++ src/pages/Earns/useLiquidityWidget.tsx
@@ -1,34 +1,46 @@
 import { ChainId, LiquidityWidget, PoolType } from 'kyberswap-liquidity-widgets'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 import { NotificationType } from 'components/Announcement/type'
 import Modal from 'components/Modal'
 import { NETWORKS_INFO } from 'constants/networks'
-import { useWeb3React } from 'hooks'
-import { useNotify, useWalletModalToggle } from 'state/application/hooks'
+import { useActiveWeb3React, useWeb3React } from 'hooks'
+import { useNetworkModalToggle, useNotify, useWalletModalToggle } from 'state/application/hooks'
 
 import useFilter from './PoolExplorer/useFilter'
 
 interface LiquidityParams {
-  provider: any
   poolAddress: string
   chainId: ChainId
   source: string
   poolType: PoolType
   positionId?: string
-  onDismiss: () => void
+  onClose: () => void
   onConnectWallet: () => void
+  onSwitchChain: () => void
+  connectedAccount: {
+    address?: string | undefined
+    chainId: number
+  }
+  onSubmitTx: (txData: { from: string; to: string; value: string; data: string; gasLimit: string }) => Promise<string>
 }
 
 const useLiquidityWidget = () => {
-  const { library } = useWeb3React()
   const toggleWalletModal = useWalletModalToggle()
+  const toggleNetworkModal = useNetworkModalToggle()
   const notify = useNotify()
+  const { library } = useWeb3React()
+  const { account, chainId } = useActiveWeb3React()
   const { filters } = useFilter()
 
-  const [liquidityParams, setLiquidityParams] = useState<LiquidityParams | null>(null)
+  const [liquidityPoolParams, setLiquidityPoolParams] = useState<{
+    poolAddress: string
+    chainId: ChainId
+    poolType: PoolType
+    positionId?: string
+  } | null>(null)
 
-  const handleCloseZapInWidget = () => setLiquidityParams(null)
+  const handleCloseZapInWidget = () => setLiquidityPoolParams(null)
   const handleOpenZapInWidget = (
     pool: { exchange: string; chainId?: number; address: string },
     positionId?: string,
@@ -50,17 +62,37 @@ const useLiquidityWidget = () => {
       )
       return
     }
-    setLiquidityParams({
-      provider: library,
+    setLiquidityPoolParams({
       poolAddress: pool.address,
       chainId: (pool.chainId || filters.chainId) as ChainId,
-      source: 'kyberswap-demo-zap',
       poolType: PoolType[`DEX_${dex.toUpperCase()}V3` as keyof typeof PoolType],
       positionId,
-      onDismiss: handleCloseZapInWidget,
-      onConnectWallet: toggleWalletModal,
     })
   }
+
+  const liquidityParams: LiquidityParams | null = useMemo(
+    () =>
+      liquidityPoolParams
+        ? {
+            ...liquidityPoolParams,
+            source: 'kyberswap-demo-zap',
+            connectedAccount: {
+              address: account,
+              chainId: chainId,
+            },
+            onClose: handleCloseZapInWidget,
+            onConnectWallet: toggleWalletModal,
+            onSwitchChain: toggleNetworkModal,
+            onSubmitTx: async (txData: { from: string; to: string; data: string; value: string; gasLimit: string }) => {
+              if (!library) throw new Error('Library is not ready!')
+              const res = await library?.getSigner().sendTransaction(txData)
+              if (!res) throw new Error('Transaction failed')
+              return res.hash
+            },
+          }
+        : null,
+    [account, chainId, library, liquidityPoolParams, toggleNetworkModal, toggleWalletModal],
+  )
 
   const liquidityWidget = liquidityParams ? (
     <Modal isOpen mobileFullWidth maxWidth={760} width={'760px'} onDismiss={handleCloseZapInWidget}>
