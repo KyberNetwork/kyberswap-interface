50889641b07a1f775f94d67fe54858f0c24c4167
viet-nv <vietnv1304@gmail.com>
2025-01-22 13:52:31 +0700

    feat: affiliate

--- src/hooks/useAffiliate.ts
+++ src/hooks/useAffiliate.ts
@@ -1,7 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import { AFFILIATE_SERVICE_URL } from 'constants/env'
+import { useWeb3React } from 'hooks'
+import { useWalletModalToggle } from 'state/application/hooks'
 
 import { useAuth } from './useAuth'
 
@@ -9,13 +11,26 @@ export const useAffiliate = () => {
   const [searchParams] = useSearchParams()
   const refCode = searchParams.get('refCode')
 
+  const { account } = useWeb3React()
+  const toggleWalletModal = useWalletModalToggle()
+
   const { accessToken, login } = useAuth()
+
+  const ref = useRef(false)
 
   useEffect(() => {
     if (!refCode) return
+
+    // connect wallet first
+    if (!account) {
+      toggleWalletModal()
+      return
+    }
+
     if (!accessToken) {
       login()
-    } else {
+    } else if (!ref.current) {
+      ref.current = true
       fetch(`${AFFILIATE_SERVICE_URL}/v1/public/affiliates/session`, {
         method: 'POST',
         headers: {
@@ -27,5 +42,6 @@ export const useAffiliate = () => {
         .then(res => res.json())
         .then(console.log)
     }
-  }, [refCode, login, accessToken])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refCode, accessToken, account, login])
 }
