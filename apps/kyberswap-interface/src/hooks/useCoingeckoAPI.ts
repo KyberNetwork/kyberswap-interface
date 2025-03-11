8ca28405226459558bc71257a118638ce086adf8
viet-nv <vietnv1304@gmail.com>
2025-03-11 17:03:20 +0700

    tmp

--- apps/kyberswap-interface/src/hooks/useCoingeckoAPI.ts
+++
@@ -1,7 +0,0 @@
-import { COINGECKO_API_URL } from 'constants/index'
-
-export default function useCoingeckoAPI() {
-  return COINGECKO_API_URL
-  // const { authenticationSuccess } = useSessionInfo()
-  // return authenticationSuccess ? COINGECKO_BFF_API_URL : COINGECKO_API_URL
-}
--- apps/kyberswap-interface/src/hooks/useParsedQueryString.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react'
-import { useLocation } from 'react-router-dom'
-
-import { queryStringToObject } from 'utils/string'
-
-export default function useParsedQueryString<T extends { [key: string]: string }>(): Partial<T> {
-  const { search } = useLocation()
-  return useMemo(() => (search && search.length > 1 ? queryStringToObject(search) : {}) as Partial<T>, [search])
-}
