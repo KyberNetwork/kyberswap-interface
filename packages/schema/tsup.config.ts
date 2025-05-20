392b17b044fdeec842f8fc1e671c4cbeab73ae68
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-05-20 16:06:31 +0700

    chore: config to build image imported to base64

---
+++ packages/schema/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts", "src/zap.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+  loader: {
+    ".png": "dataurl",
+    ".jpg": "dataurl",
+    ".jpeg": "dataurl",
+    ".gif": "dataurl",
+    ".svg": "dataurl",
+  },
+});
