53013f5c5c0321c2ce6f5e31956c26fbbe94e1d7
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-06-10 13:49:46 +0700

    fix(interface): claim modal show wrapped token

--- apps/kyberswap-interface/src/pages/Earns/hooks/useCollectFees.tsx
+++ apps/kyberswap-interface/src/pages/Earns/hooks/useCollectFees.tsx
@@ -2,7 +2,6 @@ import { t } from '@lingui/macro'
 import { useCallback, useEffect, useState } from 'react'
 
 import { NotificationType } from 'components/Announcement/type'
-import { NativeToken } from 'constants/networks/type'
 import { useActiveWeb3React, useWeb3React } from 'hooks'
 import ClaimModal, { ClaimInfo, ClaimType } from 'pages/Earns/components/ClaimModal'
 import { CoreProtocol, DEXES_SUPPORT_COLLECT_FEE } from 'pages/Earns/constants'
@@ -64,9 +63,6 @@ const useCollectFees = ({ refetchAfterCollect }: { refetchAfterCollect: () => vo
 
     setTxHash(txHash)
 
-    const nativeToken = claimInfo.nativeToken as NativeToken
-    const token0Symbol = isUniv4 ? token0.symbol : token0.isNative ? nativeToken.symbol : token0.symbol
-    const token1Symbol = isUniv4 ? token1.symbol : token1.isNative ? nativeToken.symbol : token1.symbol
     addTransactionWithType({
       type: TRANSACTION_TYPE.COLLECT_FEE,
       hash: txHash,
@@ -75,11 +71,11 @@ const useCollectFees = ({ refetchAfterCollect }: { refetchAfterCollect: () => vo
         tokenAmountOut: formatDisplayNumber(token1.amount, { significantDigits: 4 }),
         tokenAddressIn: token0.address,
         tokenAddressOut: token1.address,
-        tokenSymbolIn: token0Symbol,
-        tokenSymbolOut: token1Symbol,
+        tokenSymbolIn: token0.symbol,
+        tokenSymbolOut: token1.symbol,
         arbitrary: {
-          token_1: token0Symbol,
-          token_2: token1Symbol,
+          token_1: token0.symbol,
+          token_2: token1.symbol,
           token_1_amount: formatDisplayNumber(token0.amount, { significantDigits: 4 }),
           token_2_amount: formatDisplayNumber(token1.amount, { significantDigits: 4 }),
         },
@@ -94,14 +90,24 @@ const useCollectFees = ({ refetchAfterCollect }: { refetchAfterCollect: () => vo
 
   const onOpenClaim = (position: ParsedPosition) => {
     setOpenClaimModal(true)
+    const isUniV3 = isForkFrom(position.dex.id, CoreProtocol.UniswapV3)
+    const { token0, token1, pool } = position
+    const { nativeToken } = pool
+
+    const token0Logo = isUniV3 && token0.isNative ? nativeToken.logo : token0.logo
+    const token1Logo = isUniV3 && token1.isNative ? nativeToken.logo : token1.logo
+
+    const token0Symbol = isUniV3 && token0.isNative ? nativeToken.symbol : token0.symbol
+    const token1Symbol = isUniV3 && token1.isNative ? nativeToken.symbol : token1.symbol
+
     setClaimInfo({
       nftId: position.tokenId,
       dex: position.dex.id,
       chainId: position.chain.id,
       tokens: [
         {
-          logo: position.token0.logo,
-          symbol: position.token0.symbol,
+          logo: token0Logo,
+          symbol: token0Symbol,
           amount: position.token0.unclaimedAmount,
           value: position.token0.unclaimedValue,
           address: position.token0.address,
@@ -109,8 +115,8 @@ const useCollectFees = ({ refetchAfterCollect }: { refetchAfterCollect: () => vo
           balance: position.token0.unclaimedBalance,
         },
         {
-          logo: position.token1.logo,
-          symbol: position.token1.symbol,
+          logo: token1Logo,
+          symbol: token1Symbol,
           amount: position.token1.unclaimedAmount,
           value: position.token1.unclaimedValue,
           address: position.token1.address,
