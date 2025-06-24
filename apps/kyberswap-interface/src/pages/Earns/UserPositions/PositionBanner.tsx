a27a0c3547db737e0277cc2693e815955c599f9c
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-06-24 17:57:42 +0700

    fix(interface): small issues

--- apps/kyberswap-interface/src/pages/Earns/UserPositions/PositionBanner.tsx
+++ apps/kyberswap-interface/src/pages/Earns/UserPositions/PositionBanner.tsx
@@ -116,7 +116,7 @@ export default function PositionBanner({
                 <Text
                   fontSize={24}
                   color={totalFeeValue && totalFeeValue > 0 ? theme.primary : theme.text}
-                  sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '120px' }}
+                  sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '140px' }}
                 >
                   {formatDisplayNumber(totalFeeValue, { style: 'currency', significantDigits: 4 })}
                 </Text>
@@ -129,7 +129,7 @@ export default function PositionBanner({
               {initialLoading ? (
                 <BannerSkeleton width={90} height={28} />
               ) : (
-                <Text fontSize={24} sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '120px' }}>
+                <Text fontSize={24} sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '140px' }}>
                   {formatDisplayNumber(totalEarnedFee, { style: 'currency', significantDigits: 4 })}
                 </Text>
               )}
@@ -141,7 +141,7 @@ export default function PositionBanner({
               {initialLoading ? (
                 <BannerSkeleton width={90} height={28} />
               ) : (
-                <Text fontSize={24} sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '120px' }}>
+                <Text fontSize={24} sx={{ ...LIMIT_TEXT_STYLES, maxWidth: '140px' }}>
                   {formatDisplayNumber(totalUnclaimedFee, { style: 'currency', significantDigits: 4 })}
                 </Text>
               )}
