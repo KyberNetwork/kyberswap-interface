6df84d24f694b14db03b6c9dcb0388fa2182624a
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-07-02 15:32:46 +0700

    test: switch API to test

--- packages/zap-out-widgets/src/constants/index.ts
+++ packages/zap-out-widgets/src/constants/index.ts
@@ -161,9 +161,9 @@ export const poolTypeToDexId: { [poolType in PoolType]: number } = {
 export const PATHS = {
   BFF_API: "https://bff.kyberswap.com/api",
   KYBERSWAP_SETTING_API: "https://ks-setting.kyberswap.com/api",
-  ZAP_API: "https://zap-api.kyberengineering.io", // https://pre-zap-api.kyberengineering.io  https://zap-api.kyberswap.com
+  ZAP_API: "https://pre-zap-api.kyberengineering.io", // https://pre-zap-api.kyberengineering.io  https://zap-api.kyberswap.com
   COINGECKO_API_URL: "https://api.coingecko.com/api/v3",
   GO_PLUS_API: "https://api.gopluslabs.io/api/v1/token_security",
-  ZAP_EARN_API: "https://pre-zap-earn-service.kyberengineering.io/api",
+  ZAP_EARN_API: "https://pre-zap-earn-univ4-service.kyberengineering.io/api",
   TOKEN_API: "https://token-api.kyberengineering.io/api",
 };
