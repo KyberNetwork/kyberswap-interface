af68db0801c495991fa539e9398adae322ba608b
viet-nv <vietnv1304@gmail.com>
2025-07-30 12:49:52 +0700

    feat: show zap in pos fee

--- packages/liquidity-widgets/src/components/Content/ZapSummary.tsx
+++ packages/liquidity-widgets/src/components/Content/ZapSummary.tsx
@@ -2,10 +2,18 @@ import { useMemo, useState } from 'react';
 
 import { useShallow } from 'zustand/shallow';
 
-import { AddLiquidityAction, DEXES_INFO, PoolType, ZapAction, defaultToken } from '@kyber/schema';
+import {
+  AddLiquidityAction,
+  DEXES_INFO,
+  PoolType,
+  RemoveLiquidityAction,
+  Token,
+  ZapAction,
+  defaultToken,
+} from '@kyber/schema';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@kyber/ui';
 import { parseSwapActions } from '@kyber/utils';
-import { formatWei } from '@kyber/utils/number';
+import { formatTokenAmount, formatWei } from '@kyber/utils/number';
 
 import { useZapState } from '@/hooks/useZapState';
 import { usePoolStore } from '@/stores/usePoolStore';
@@ -56,6 +64,22 @@ export default function ZapSummary() {
     return { addedAmount0, addedAmount1 };
   }, [pool, zapInfo?.zapDetails.actions]);
 
+  const actionRemoveLiq = zapInfo?.zapDetails.actions.find(item => item.type === ZapAction.REMOVE_LIQUIDITY) as
+    | RemoveLiquidityAction
+    | undefined;
+
+  const { fees } = actionRemoveLiq?.removeLiquidity || {};
+
+  const poolTokens: Token[] = pool === 'loading' ? [] : [pool.token0, pool.token1];
+
+  const feeToken0 = poolTokens.find(item => item.address.toLowerCase() === fees?.[0]?.address.toLowerCase());
+  const feeToken1 = poolTokens.find(item => item.address.toLowerCase() === fees?.[1]?.address.toLowerCase());
+
+  const feeAmount0 = BigInt(fees?.[0]?.amount || 0);
+  const feeAmount1 = BigInt(fees?.[1]?.amount || 0);
+
+  const hasFee = feeAmount0 !== 0n || feeAmount1 !== 0n;
+
   return (
     <>
       <Accordion type="single" collapsible className="w-full mt-4" value={expanded ? 'item-1' : ''}>
@@ -70,13 +94,25 @@ export default function ZapSummary() {
           </AccordionTrigger>
           <AccordionContent className="px-4 pb-4 pt-0 border border-stroke !border-t-0 rounded-b-md">
             <p className="text-subText text-xs italic">The actual Zap Routes could be adjusted with on-chain states</p>
-
             <div className="h-[1px] w-full bg-stroke mt-1 mb-3" />
-
+            {hasFee && (
+              <div className="flex gap-3 items-center mt-3 text-xs">
+                <div className="rounded-full w-6 h-6 flex items-center justify-center font-medium bg-layer2">1</div>
+                <div className="flex-1 text-subText leading-4">
+                  <>
+                    Claim fee {feeAmount0 !== 0n ? formatTokenAmount(feeAmount0, feeToken0?.decimals || 18) : ''}{' '}
+                    {feeAmount0 !== 0n ? feeToken0?.symbol : ''}{' '}
+                    {feeAmount1 !== 0n
+                      ? `+ ${formatTokenAmount(feeAmount1, feeToken1?.decimals || 18)} ${feeToken1?.symbol}`
+                      : ''}{' '}
+                  </>
+                </div>
+              </div>
+            )}
             {swapActions.map((item, index) => (
               <div className="flex gap-3 items-center mt-3 text-xs" key={index}>
                 <div className="rounded-full w-6 h-6 flex items-center justify-center font-medium bg-layer2">
-                  {index + 1}
+                  {index + (hasFee ? 2 : 1)}
                 </div>
                 <div className="flex-1 text-subText leading-4">
                   Swap {item.amountIn} {item.tokenInSymbol} for {item.amountOut} {item.tokenOutSymbol} via{' '}
@@ -84,10 +120,9 @@ export default function ZapSummary() {
                 </div>
               </div>
             ))}
-
             <div className="flex gap-3 items-center text-xs mt-3">
               <div className="rounded-full w-6 h-6 flex items-center justify-center font-medium bg-layer2">
-                {swapActions.length + 1}
+                {swapActions.length + (hasFee ? 2 : 1)}
               </div>
               <div className="flex-1 text-subText leading-4">
                 Build LP using {addedLiquidityInfo.addedAmount0} {symbol0} and {addedLiquidityInfo.addedAmount1}{' '}
