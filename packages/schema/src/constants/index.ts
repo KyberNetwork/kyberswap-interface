261432b47c4fe22c2784fc7cd1ebeebb2ff4a8c2
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-08-13 11:50:53 +0700

    switch endpoint

--- packages/schema/src/constants/index.ts
+++ packages/schema/src/constants/index.ts
@@ -30,11 +30,12 @@ export const CHAIN_ID_TO_CHAIN: { [chainId in ChainId]: string } = {
 export const API_URLS = {
   BFF_API: 'https://bff.kyberswap.com/api',
   KYBERSWAP_SETTING_API: 'https://ks-setting.kyberswap.com/api',
-  // ZAP_API: 'https://zap-api.kyberswap.com',
-  ZAP_API: 'https://pre-zap-api.kyberengineering.io',
+  ZAP_API: 'https://zap-api.kyberswap.com',
+  // ZAP_API: 'https://pre-zap-api.kyberengineering.io',
   COINGECKO_API_URL: 'https://api.coingecko.com/api/v3',
   GO_PLUS_API: 'https://api.gopluslabs.io/api/v1/token_security',
-  ZAP_EARN_API: 'https://pre-zap-earn-service.kyberengineering.io/api',
+  ZAP_EARN_API: 'https://zap-earn-service-v3.kyberengineering.io/api',
+  // ZAP_EARN_API: 'https://pre-zap-earn-service.kyberengineering.io/api',
   TOKEN_API: 'https://token-api.kyberengineering.io/api',
   DOCUMENT: {
     ZAP_FEE_MODEL: 'https://docs.kyberswap.com/kyberswap-solutions/kyberswap-zap-as-a-service/zap-fee-model',
