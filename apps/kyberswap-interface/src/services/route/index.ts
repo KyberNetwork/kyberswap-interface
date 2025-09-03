9c45eb3db32b918043f12f3a84a30ab3b58aeb05
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-09-03 15:17:13 +0700

    update: swap price impact

--- apps/kyberswap-interface/src/services/route/index.ts
+++ apps/kyberswap-interface/src/services/route/index.ts
@@ -2,6 +2,8 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 import { baseQueryOauthDynamic } from 'services/baseQueryOauth'
 import { BuildRoutePayload, BuildRouteResponse } from 'services/route/types/buildRoute'
 
+import { TOKEN_API_URL } from 'constants/env'
+
 import { GetRouteParams, GetRouteResponse } from './types/getRoute'
 
 const routeApi = createApi({
@@ -30,6 +32,56 @@ const routeApi = createApi({
           },
         }
       },
+      async transformResponse(baseResponse: GetRouteResponse, _meta, { params }): Promise<GetRouteResponse> {
+        const { routeSummary } = baseResponse?.data || {}
+        const { chainId, tokenInDecimals, tokenOutDecimals, tokenIn, tokenOut } = params || {}
+
+        // Ensure all necessary data is available
+        if (baseResponse?.data?.routeSummary && routeSummary && chainId && tokenInDecimals && tokenOutDecimals) {
+          const { amountIn, amountOut } = routeSummary
+
+          try {
+            const priceResponse = await fetch(`${TOKEN_API_URL}/v1/public/tokens/prices`, {
+              method: 'POST',
+              body: JSON.stringify({
+                [chainId]: [tokenIn, tokenOut],
+              }),
+            }).then(res => res.json())
+
+            const tokenInPrices = priceResponse?.data?.[chainId]?.[tokenIn]
+            const tokenInMidPrice =
+              tokenInPrices?.PriceBuy && tokenInPrices?.PriceSell
+                ? (tokenInPrices.PriceBuy + tokenInPrices.PriceSell) / 2
+                : null
+            const tokenOutPrices = priceResponse?.data?.[chainId]?.[tokenOut]
+            const tokenOutMidPrice =
+              tokenOutPrices?.PriceBuy && tokenOutPrices?.PriceSell
+                ? (tokenOutPrices.PriceBuy + tokenOutPrices.PriceSell) / 2
+                : null
+
+            return {
+              ...baseResponse,
+              data: {
+                ...baseResponse.data,
+                routeSummary: {
+                  ...routeSummary,
+                  amountInUsd: tokenInMidPrice
+                    ? ((Number(amountIn || 0) / 10 ** tokenInDecimals) * tokenInMidPrice).toString()
+                    : '',
+                  amountOutUsd: tokenOutMidPrice
+                    ? ((Number(amountOut || 0) / 10 ** tokenOutDecimals) * tokenOutMidPrice).toString()
+                    : '',
+                },
+              },
+            }
+          } catch (error) {
+            console.error('Failed to fetch on-chain price:', error)
+          }
+        }
+
+        // Return original response if conditions are not met or request fails
+        return baseResponse
+      },
     }),
     buildRoute: builder.mutation<
       BuildRouteResponse,
@@ -47,6 +99,54 @@ const routeApi = createApi({
             'x-client-id': payload.source || 'kyberswap',
           },
         }
+      },
+      async transformResponse(baseResponse: BuildRouteResponse, _meta, { payload }): Promise<BuildRouteResponse> {
+        const { data } = baseResponse || {}
+        const { chainId, tokenInDecimals, tokenOutDecimals, routeSummary } = payload || {}
+        const { tokenIn, tokenOut } = routeSummary || {}
+
+        // Ensure all necessary data is available
+        if (data && routeSummary && chainId && tokenInDecimals && tokenOutDecimals) {
+          const { amountIn, amountOut } = routeSummary
+
+          try {
+            const priceResponse = await fetch(`${TOKEN_API_URL}/v1/public/tokens/prices`, {
+              method: 'POST',
+              body: JSON.stringify({
+                [chainId]: [tokenIn, tokenOut],
+              }),
+            }).then(res => res.json())
+
+            const tokenInPrices = priceResponse?.data?.[chainId]?.[tokenIn]
+            const tokenInMidPrice =
+              tokenInPrices?.PriceBuy && tokenInPrices?.PriceSell
+                ? (tokenInPrices.PriceBuy + tokenInPrices.PriceSell) / 2
+                : null
+            const tokenOutPrices = priceResponse?.data?.[chainId]?.[tokenOut]
+            const tokenOutMidPrice =
+              tokenOutPrices?.PriceBuy && tokenOutPrices?.PriceSell
+                ? (tokenOutPrices.PriceBuy + tokenOutPrices.PriceSell) / 2
+                : null
+
+            return {
+              ...baseResponse,
+              data: {
+                ...data,
+                amountInUsd: tokenInMidPrice
+                  ? ((Number(amountIn || 0) / 10 ** tokenInDecimals) * tokenInMidPrice).toString()
+                  : '',
+                amountOutUsd: tokenOutMidPrice
+                  ? ((Number(amountOut || 0) / 10 ** tokenOutDecimals) * tokenOutMidPrice).toString()
+                  : '',
+              },
+            }
+          } catch (error) {
+            console.error('Failed to fetch on-chain price:', error)
+          }
+        }
+
+        // Return original response if conditions are not met or request fails
+        return baseResponse
       },
     }),
   }),
