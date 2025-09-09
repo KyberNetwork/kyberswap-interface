409de5850addcdeb7b6b7bf5f8cecb61180b6144
viet-nv <vietnv1304@gmail.com>
2025-09-09 14:17:45 +0700

    feat: across

--- apps/kyberswap-interface/src/pages/CrossChainSwap/adapters/AcrossAdapter.ts
+++ apps/kyberswap-interface/src/pages/CrossChainSwap/adapters/AcrossAdapter.ts
@@ -1,6 +1,5 @@
 import { AcrossClient, createAcrossClient } from '@across-protocol/app-sdk'
 import { ChainId, Currency } from '@kyberswap/ks-sdk-core'
-import { ethers } from 'ethers'
 import { WalletClient, formatUnits } from 'viem'
 import { arbitrum, base, blast, linea, mainnet, optimism, polygon, scroll, unichain, zksync } from 'viem/chains'
 
@@ -16,25 +15,6 @@ import {
   NormalizedTxResponse,
   SwapStatus,
 } from './BaseSwapAdapter'
-
-// Define the ABI of the functions
-const transferFunction = 'function transfer(address to, uint256 amount)'
-
-// Create Interface instances
-const erc20Interface = new ethers.utils.Interface([transferFunction])
-
-const multicalHandlerContract: Record<string, `0x${string}`> = {
-  [ChainId.MAINNET]: '0x924a9f036260DdD5808007E1AA95f08eD08aA569',
-  [ChainId.ARBITRUM]: '0x924a9f036260DdD5808007E1AA95f08eD08aA569',
-  [ChainId.OPTIMISM]: '0x924a9f036260DdD5808007E1AA95f08eD08aA569',
-  [ChainId.LINEA]: '0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB',
-  [ChainId.MATIC]: '0x924a9f036260DdD5808007E1AA95f08eD08aA569',
-  [ChainId.ZKSYNC]: '0x863859ef502F0Ee9676626ED5B418037252eFeb2',
-  [ChainId.BASE]: '0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB',
-  [ChainId.SCROLL]: '0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB',
-  [ChainId.BLAST]: '0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB',
-  [ChainId.UNICHAIN]: '0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB',
-}
 
 export class AcrossAdapter extends BaseSwapAdapter {
   private acrossClient: AcrossClient
@@ -77,47 +57,48 @@ export class AcrossAdapter extends BaseSwapAdapter {
 
   async getQuote(params: EvmQuoteParams): Promise<NormalizedQuote> {
     try {
-      // const res = await this.acrossClient.getSwapQuote({
-      //   route: {
-      //     originChainId: +params.fromChain,
-      //     destinationChainId: +params.toChain,
-      //     inputToken: (params.fromToken.isNative ? ZERO_ADDRESS : params.fromToken.wrapped.address) as `0x${string}`,
-      //     outputToken: (params.toToken.isNative ? ZERO_ADDRESS : params.toToken.wrapped.address) as `0x${string}`,
-      //   },
-      //   amount: params.amount,
-      //   appFee: (params.feeBps * 100) / 10_000,
-      //   appFeeRecipient: CROSS_CHAIN_FEE_RECEIVER,
-      //   slippage: (params.slippage * 100) / 10_000,
-      //   depositor: params.sender,
-      // })
-      console.log(res)
-      const resp = await this.acrossClient.getQuote({
+      const res = await this.acrossClient.getSwapQuote({
         route: {
           originChainId: +params.fromChain,
           destinationChainId: +params.toChain,
-          inputToken: params.fromToken.wrapped.address as `0x${string}`,
-          outputToken: params.toToken.wrapped.address as `0x${string}`,
-          isNative: params.fromToken.isNative,
+          inputToken: (params.fromToken.isNative ? ZERO_ADDRESS : params.fromToken.wrapped.address) as `0x${string}`,
+          outputToken: (params.toToken.isNative ? ZERO_ADDRESS : params.toToken.wrapped.address) as `0x${string}`,
         },
-        inputAmount: params.amount,
-        recipient: multicalHandlerContract[params.toChain],
-        crossChainMessage: {
-          actions: [
-            {
-              target: params.toToken.wrapped.address as `0x${string}`,
-              callData: erc20Interface.encodeFunctionData('transfer', [CROSS_CHAIN_FEE_RECEIVER, 0n]) as `0x${string}`,
-              value: '0',
-              update: updatedAmount => ({
-                callData: erc20Interface.encodeFunctionData('transfer', [
-                  CROSS_CHAIN_FEE_RECEIVER,
-                  (updatedAmount * BigInt(params.feeBps)) / 10_000n,
-                ]) as `0x${string}`,
-              }),
-            },
-          ],
-          fallbackRecipient: params.recipient as `0x${string}`,
-        },
+        amount: params.amount,
+        appFee: params.feeBps / 10_000,
+        appFeeRecipient: CROSS_CHAIN_FEE_RECEIVER,
+        slippage: (params.slippage * 100) / 10_000,
+        depositor: params.sender,
       })
+
+      // console.log(res)
+      // const resp = await this.acrossClient.getQuote({
+      //   route: {
+      //     originChainId: +params.fromChain,
+      //     destinationChainId: +params.toChain,
+      //     inputToken: params.fromToken.wrapped.address as `0x${string}`,
+      //     outputToken: params.toToken.wrapped.address as `0x${string}`,
+      //     isNative: params.fromToken.isNative,
+      //   },
+      //   inputAmount: params.amount,
+      //   recipient: multicalHandlerContract[params.toChain],
+      //   crossChainMessage: {
+      //     actions: [
+      //       {
+      //         target: params.toToken.wrapped.address as `0x${string}`,
+      //         callData: erc20Interface.encodeFunctionData('transfer', [CROSS_CHAIN_FEE_RECEIVER, 0n]) as `0x${string}`,
+      //         value: '0',
+      //         update: updatedAmount => ({
+      //           callData: erc20Interface.encodeFunctionData('transfer', [
+      //             CROSS_CHAIN_FEE_RECEIVER,
+      //             (updatedAmount * BigInt(params.feeBps)) / 10_000n,
+      //           ]) as `0x${string}`,
+      //         }),
+      //       },
+      //     ],
+      //     fallbackRecipient: params.recipient as `0x${string}`,
+      //   },
+      // })
 
       // across only have bridge then we can treat token in and out price usd are the same in case price service is not supported
       const isSameToken = params.fromToken.symbol === params.toToken.symbol
@@ -129,8 +110,8 @@ export class AcrossAdapter extends BaseSwapAdapter {
         isSameToken && NOT_SUPPORTED_CHAINS_PRICE_SERVICE.includes(params.toChain) && params.tokenInUsd
           ? params.tokenInUsd
           : params.tokenOutUsd
-      const feeAmount = (BigInt(resp.deposit.outputAmount) * BigInt(params.feeBps)) / 10_000n
-      const formattedOutputAmount = formatUnits(BigInt(resp.deposit.outputAmount) - feeAmount, params.toToken.decimals)
+
+      const formattedOutputAmount = formatUnits(BigInt(res.expectedOutputAmount), params.toToken.decimals)
       const formattedInputAmount = formatUnits(BigInt(params.amount), params.fromToken.decimals)
 
       const inputUsd = tokenInUsd * +formattedInputAmount
@@ -138,17 +119,17 @@ export class AcrossAdapter extends BaseSwapAdapter {
 
       return {
         quoteParams: params,
-        outputAmount: BigInt(resp.deposit.outputAmount) - feeAmount,
+        outputAmount: BigInt(res.expectedOutputAmount),
         formattedOutputAmount,
         inputUsd: tokenInUsd * +formatUnits(BigInt(params.amount), params.fromToken.decimals),
         outputUsd: tokenOutUsd * +formattedOutputAmount,
         rate: +formattedOutputAmount / +formattedInputAmount,
-        timeEstimate: resp.estimatedFillTimeSec,
+        timeEstimate: res.expectedFillTime,
         priceImpact: !inputUsd || !outputUsd ? NaN : ((inputUsd - outputUsd) * 100) / inputUsd,
         // TODO: what is gas fee for across
         gasFeeUsd: 0,
-        contractAddress: resp.deposit.spokePoolAddress,
-        rawQuote: resp,
+        contractAddress: res.checks.allowance.spender,
+        rawQuote: res,
 
         protocolFee: 0,
         platformFeePercent: (params.feeBps * 100) / 10_000,
@@ -162,11 +143,11 @@ export class AcrossAdapter extends BaseSwapAdapter {
   async executeSwap(quote: Quote, walletClient: WalletClient): Promise<NormalizedTxResponse> {
     return new Promise<NormalizedTxResponse>((resolve, reject) => {
       this.acrossClient
-        .executeQuote({
+        .executeSwapQuote({
           walletClient: walletClient as any,
-          deposit: quote.quote.rawQuote.deposit,
+          swapQuote: quote.quote.rawQuote as any,
           onProgress: progress => {
-            if (progress.step === 'deposit' && 'txHash' in progress) {
+            if (progress.step === 'swap' && 'txHash' in progress) {
               resolve({
                 sender: quote.quote.quoteParams.sender,
                 sourceTxHash: progress.txHash,
