17977081a5d5eaeb43bc042b15de6372d8598d37
Khac Kien <ii.khackien@gmail.com>
2025-10-20 15:47:54 +0700

    APR estimation

--- packages/schema/src/constants/index.ts
+++ packages/schema/src/constants/index.ts
@@ -13,8 +13,8 @@ export const API_URLS = {
   // ZAP_API: 'https://pre-zap-api.kyberengineering.io',
   COINGECKO_API_URL: 'https://api.coingecko.com/api/v3',
   GO_PLUS_API: 'https://api.gopluslabs.io/api/v1/token_security',
-  ZAP_EARN_API: 'https://zap-earn-service-v3.kyberengineering.io/api',
-  // ZAP_EARN_API: 'https://pre-zap-earn-service.kyberengineering.io/api',
+  // ZAP_EARN_API: 'https://zap-earn-service-v3.kyberengineering.io/api',
+  ZAP_EARN_API: 'https://pre-zap-earn-service.kyberengineering.io/api',
   TOKEN_API: 'https://token-api.kyberengineering.io/api',
   DOCUMENT: {
     ZAP_FEE_MODEL: 'https://docs.kyberswap.com/kyberswap-solutions/kyberswap-zap-as-a-service/zap-fee-model',
