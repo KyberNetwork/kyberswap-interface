93a7599d91603c4d927c9f519a7d16e6de7e63c9
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2025-10-24 16:01:13 +0700

    fix wording

--- apps/kyberswap-interface/src/pages/Earns/constants/chains/bsc.ts
+++ apps/kyberswap-interface/src/pages/Earns/constants/chains/bsc.ts
@@ -1,8 +1,8 @@
-import bscIcon from 'assets/networks/bnb.svg'
+import bscLogo from 'assets/networks/bnb.svg'
 
 export default {
   nativeAddress: '0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
   farmingSupported: true,
   univ4StateViewContract: '0xd13dd3d6e93f276fafc9db9e6bb47c1180aee0c4',
-  logo: bscIcon,
+  logo: bscLogo,
 }
--- apps/kyberswap-interface/src/pages/Earns/constants/chains/optimism.ts
+++ apps/kyberswap-interface/src/pages/Earns/constants/chains/optimism.ts
@@ -1,9 +1,9 @@
-import optimismIcon from 'assets/networks/optimism.svg'
+import optimismLogo from 'assets/networks/optimism.svg'
 import { ETHER_ADDRESS } from 'constants/index'
 
 export default {
   nativeAddress: ETHER_ADDRESS.toLowerCase(),
   farmingSupported: false,
   univ4StateViewContract: '0xc18a3169788f4f75a170290584eca6395c75ecdb',
-  logo: optimismIcon,
+  logo: optimismLogo,
 }
