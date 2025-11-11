62301c86ab4ef04366b3f7fa9e67e2c2b25ec2ac
Khac Kien <ii.khackien@gmail.com>
2025-11-11 11:47:36 +0700

    Update Preview

--- packages/liquidity-widgets/src/components/Preview/PriceInfo.tsx
+++ packages/liquidity-widgets/src/components/Preview/PriceInfo.tsx
@@ -1,16 +1,14 @@
 import { Trans } from '@lingui/macro';
 
 import { Pool, univ3PoolNormalize } from '@kyber/schema';
-import { TokenSymbol } from '@kyber/ui';
-import { formatDisplayNumber } from '@kyber/utils/number';
 
-import SwitchIcon from '@/assets/svg/switch.svg';
+import PriceInfoContent from '@/components/Content/PriceInfo';
 import { useZapState } from '@/hooks/useZapState';
 import { usePoolStore } from '@/stores/usePoolStore';
 import { getPriceRangeToShow } from '@/utils';
 
 export default function PriceInfo({ pool }: { pool: Pool }) {
-  const { toggleRevertPrice, poolPrice, revertPrice } = usePoolStore(['toggleRevertPrice', 'poolPrice', 'revertPrice']);
+  const { revertPrice } = usePoolStore(['revertPrice']);
   const { success: isUniV3 } = univ3PoolNormalize.safeParse(pool);
   const { tickLower, tickUpper, minPrice, maxPrice } = useZapState();
 
@@ -23,59 +21,37 @@ export default function PriceInfo({ pool }: { pool: Pool }) {
     maxPrice,
   });
 
-  const quoteTokenSymbol = (
-    <Trans>
-      <TokenSymbol symbol={!revertPrice ? pool?.token1.symbol : pool?.token0.symbol} maxWidth={80} />
-      <span>per</span>
-      <TokenSymbol symbol={!revertPrice ? pool?.token0.symbol : pool?.token1.symbol} maxWidth={80} />
-    </Trans>
-  );
-
-  return isUniV3 ? (
-    <div className="ks-lw-card border border-stroke bg-transparent mt-4 text-sm">
-      <div className="flex justify-between items-center gap-4 w-full">
-        <div className="ks-lw-card-title">
-          <Trans>Current pool price</Trans>
-        </div>
-        <div className="flex items-center gap-1 text-sm">
-          <span>{formatDisplayNumber(poolPrice, { significantDigits: 6 })}</span>
-          <div className="flex items-center gap-1">{quoteTokenSymbol}</div>
-          <SwitchIcon className="cursor-pointer" onClick={() => toggleRevertPrice()} role="button" />
-        </div>
-      </div>
+  if (!isUniV3) return null;
+  return (
+    <div className="ks-lw-card border border-stroke bg-transparent text-sm">
+      <PriceInfoContent flatten />
 
       {priceRange && (
         <div className="flex justify-between items-center gap-4 w-full mt-2">
-          <div className="ks-lw-card flex flex-col gap-[6px] items-center flex-1 w-1/2">
+          <div className="ks-lw-card flex flex-col items-center flex-1 w-1/2">
             <div className="ks-lw-card-title">
               <Trans>Min Price</Trans>
             </div>
             <div
               title={priceRange?.minPrice?.toString()}
-              className="overflow-hidden text-ellipsis whitespace-nowrap w-full text-center"
+              className="w-full text-center text-base overflow-hidden text-ellipsis whitespace-nowrap"
             >
               {priceRange?.minPrice}
             </div>
-            <div className="ks-lw-card-title">
-              <div className="flex items-center gap-1">{quoteTokenSymbol}</div>
-            </div>
           </div>
-          <div className="ks-lw-card flex flex-col gap-[6px] items-center flex-1 w-1/2">
+          <div className="ks-lw-card flex flex-col items-center flex-1 w-1/2">
             <div className="ks-lw-card-title">
               <Trans>Max Price</Trans>
             </div>
             <div
               title={priceRange?.maxPrice?.toString()}
-              className="text-center w-full overflow-hidden text-ellipsis whitespace-nowrap"
+              className="w-full text-center text-base overflow-hidden text-ellipsis whitespace-nowrap"
             >
               {priceRange?.maxPrice}
-            </div>
-            <div className="ks-lw-card-title">
-              <div className="flex items-center gap-1">{quoteTokenSymbol}</div>
             </div>
           </div>
         </div>
       )}
     </div>
-  ) : null;
+  );
 }
