5e83e2219f62656270eccba179059f65661f58e9
Kane <dinhtiennguyen.1202@gmail.com>
2025-11-19 17:02:57 +0700

    feat: enable farming on arb (#2863)

--- apps/kyberswap-interface/src/pages/Earns/constants/chains/arbitrum.ts
+++ apps/kyberswap-interface/src/pages/Earns/constants/chains/arbitrum.ts
@@ -3,7 +3,7 @@ import { ETHER_ADDRESS } from 'constants/index'
 
 export default {
   nativeAddress: ETHER_ADDRESS.toLowerCase(),
-  farmingSupported: false,
+  farmingSupported: true,
   univ4StateViewContract: '0x76fd297e2d437cd7f76d50f01afe6160f86e9990',
   logo: arbitrumLogo,
 }
