c11a74c36c72464fcf32bcb994a1da581dc0db92
Hoang Trinh <hoang.trinhj@gmail.com>
2025-12-02 14:21:29 +0700

    feat: add kyberacross provider

--- apps/kyberswap-interface/src/pages/CrossChainSwap/hooks/useCrossChainSwap.tsx
+++ apps/kyberswap-interface/src/pages/CrossChainSwap/hooks/useCrossChainSwap.tsx
@@ -811,6 +811,8 @@ export const CrossChainSwapRegistryProvider = ({ children }: { children: React.R
                       toToken: params.toToken,
                       publicKey: params.publicKey,
                       walletClient: params.walletClient,
+                      sender: params.sender,
+                      recipient: params.recipient,
                     },
                     outputAmount: BigInt(data.outputAmount),
                     formattedOutputAmount: data.formattedOutputAmount,
