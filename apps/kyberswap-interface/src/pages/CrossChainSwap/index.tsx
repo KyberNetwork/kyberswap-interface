68a41153b6fc058c5b17ea123101baeb20a9d3f4
Khac Kien <ii.khackien@gmail.com>
2026-01-15 18:58:55 +0700

    QuoteSteps first look

--- apps/kyberswap-interface/src/pages/CrossChainSwap/index.tsx
+++ apps/kyberswap-interface/src/pages/CrossChainSwap/index.tsx
@@ -27,6 +27,7 @@ import { TokenLogoWithChain } from 'pages/CrossChainSwap/components/TokenLogoWit
 import { TokenPanel } from 'pages/CrossChainSwap/components/TokenPanel'
 import useAcceptTermAndPolicy from 'pages/CrossChainSwap/hooks/useAcceptTermAndPolicy'
 import { CrossChainSwapRegistryProvider, useCrossChainSwap } from 'pages/CrossChainSwap/hooks/useCrossChainSwap'
+import { Quote } from 'pages/CrossChainSwap/registry'
 import { NearToken, SolanaToken } from 'state/crossChainSwap'
 import { isEvmChain } from 'utils'
 import { formatDisplayNumber } from 'utils/numbers'
@@ -37,7 +38,11 @@ const Wrapper = styled.div`
   gap: 1rem;
 `
 
-function CrossChainSwap() {
+type CrossChainSwapProps = {
+  onQuoteChange?: (quote: Quote) => void
+}
+
+export function CrossChainSwap({ onQuoteChange }: CrossChainSwapProps) {
   const {
     amount,
     setAmount,
@@ -71,6 +76,12 @@ function CrossChainSwap() {
   const isToEvm = toChainId && isEvmChain(toChainId)
   const isToSolana = toChainId === NonEvmChain.Solana
   const networkName = isToNear ? 'NEAR' : isToBtc ? 'Bitcoin' : isToSolana ? 'Solana' : 'EVM'
+
+  useEffect(() => {
+    if (selectedQuote) {
+      onQuoteChange?.(selectedQuote)
+    }
+  }, [onQuoteChange, selectedQuote])
 
   useEffect(() => {
     if (isEvmChain(fromChainId) && isToEvm && !showEvmRecipient) {
@@ -319,6 +330,7 @@ function CrossChainSwap() {
               selectedQuote={selectedQuote}
               onChange={newSelectedQuote => {
                 setSelectedAdapter(newSelectedQuote.adapter.getName())
+                onQuoteChange?.(newSelectedQuote)
               }}
               tokenOut={currencyOut}
             />
@@ -352,10 +364,10 @@ function CrossChainSwap() {
   )
 }
 
-export default function CrossChainSwapPage() {
+export default function CrossChainSwapPage(props: CrossChainSwapProps) {
   return (
     <CrossChainSwapRegistryProvider>
-      <CrossChainSwap />
+      <CrossChainSwap {...props} />
     </CrossChainSwapRegistryProvider>
   )
 }
