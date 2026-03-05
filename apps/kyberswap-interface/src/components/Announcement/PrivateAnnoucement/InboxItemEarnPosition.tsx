d6b65700318059bec6c699cdb00f69fff1bd9f99
Khac Kien <kien.nguyen@kyber.network>
2026-03-05 08:58:39 +0700

    feat: update notif earn status (#2977)
    * First look
    
    Remove mock
    
    * Hide in/out range
    
    * Update balance changes
    
    * Catch chainId
    
    * Update env pre
    
    * Update text removed
    
    * Fix
    
    * Catch more templateBody
    
    * Update env

--- apps/kyberswap-interface/src/components/Announcement/PrivateAnnoucement/InboxItemEarnPosition.tsx
+++ apps/kyberswap-interface/src/components/Announcement/PrivateAnnoucement/InboxItemEarnPosition.tsx
@@ -1,7 +1,9 @@
 import { ChainId } from '@kyberswap/ks-sdk-core'
 import { t } from '@lingui/macro'
+import { rgba } from 'polished'
+import { useMemo } from 'react'
 import { useNavigate } from 'react-router'
-import { Flex } from 'rebass'
+import { Flex, Text } from 'rebass'
 
 import { PrivateAnnouncementProp } from 'components/Announcement/PrivateAnnoucement'
 import InboxIcon from 'components/Announcement/PrivateAnnoucement/Icon'
@@ -12,47 +14,112 @@ import {
   InboxItemWrapper,
   PrimaryText,
   RowItem,
-  StatusBadge,
   Title,
 } from 'components/Announcement/PrivateAnnoucement/styled'
-import { AnnouncementTemplatePoolPosition } from 'components/Announcement/type'
+import {
+  AnnouncementTemplatePoolPosition,
+  PoolPositionAnnouncement,
+  PoolPositionLiquidityAnnouncement,
+} from 'components/Announcement/type'
 import { DoubleCurrencyLogoV2 } from 'components/DoubleLogo'
+import TokenLogo from 'components/TokenLogo'
 import { APP_PATHS } from 'constants/index'
 import useTheme from 'hooks/useTheme'
+import { EARN_DEXES, Exchange } from 'pages/Earns/constants'
+import { getDexVersion, getTokenId } from 'pages/Earns/utils/position'
+import { formatDisplayNumber } from 'utils/numbers'
 
-function InboxItemEarnPosition({
+enum Status {
+  NEW,
+  ADD,
+  FULL_REMOVE,
+  PARTIAL_REMOVE,
+  IN_RANGE,
+  OUT_RANGE,
+}
+
+const InboxItemEarnPosition = ({
   announcement,
   onRead,
   style,
   time,
-  title,
   onPin,
   onDelete,
-}: PrivateAnnouncementProp<AnnouncementTemplatePoolPosition>) {
-  const { templateBody, isRead, templateType } = announcement
+}: PrivateAnnouncementProp<AnnouncementTemplatePoolPosition>) => {
   const theme = useTheme()
+  const navigate = useNavigate()
+
+  const { isRead, templateType, templateBody } = announcement
+  const position = (normalizePosition(templateBody.position) || {}) as PoolPositionLiquidityAnnouncement<number>
 
   const {
     positionId,
-    currentPrice,
-    maxPrice,
-    minPrice,
-    token0LogoURL,
-    token0Symbol,
-    token1LogoURL,
-    token1Symbol,
     chainId: rawChain,
+    token0Symbol,
+    token1Symbol,
+    token0LogoURL,
+    token1LogoURL,
+    currentPrice,
+    minPrice,
+    maxPrice,
     exchange,
-  } = templateBody?.position || {}
+    notificationType,
+    oldValueUsd,
+    newValueUsd,
+    oldToken0Amount,
+    newToken0Amount,
+    oldToken1Amount,
+    newToken1Amount,
+    token0Decimals,
+    token1Decimals,
+  } = position
 
   const chainId = Number(rawChain) as ChainId
-  const isInRange = currentPrice >= minPrice && currentPrice <= maxPrice
-  const statusMessage = isInRange ? t`Back in range` : t`Out of range`
 
-  const navigate = useNavigate()
+  const [statusMessage, status] = useMemo(() => {
+    if (!notificationType) {
+      const isInRange = currentPrice >= minPrice && currentPrice <= maxPrice
+      return isInRange ? [t`Position Back in range`, Status.IN_RANGE] : [t`Position Out of range`, Status.OUT_RANGE]
+    }
+    if (notificationType === 'CREATED' || oldValueUsd === 0) return [t`Position Created`, Status.NEW]
+    if (notificationType === 'LIQUIDITY_INCREASED' || newValueUsd > oldValueUsd)
+      return [t`Position Increased`, Status.ADD]
+    if (notificationType === 'LIQUIDITY_DECREASED' || newValueUsd < oldValueUsd) {
+      return newValueUsd === 0
+        ? [t`Position Removed`, Status.FULL_REMOVE]
+        : [t`Position Partially Removed`, Status.PARTIAL_REMOVE]
+    }
+    return [t`Position Updated`, Status.NEW]
+  }, [notificationType, currentPrice, minPrice, maxPrice, oldValueUsd, newValueUsd])
+
+  const dex = useMemo(() => {
+    return {
+      id: exchange,
+      name: EARN_DEXES[exchange].name,
+      logo: EARN_DEXES[exchange].logo,
+      version: getDexVersion(exchange as Exchange),
+    }
+  }, [exchange])
+
+  const token0Before = oldToken0Amount / 10 ** token0Decimals
+  const token0After = newToken0Amount / 10 ** token0Decimals
+  const token1Before = oldToken1Amount / 10 ** token1Decimals
+  const token1After = newToken1Amount / 10 ** token1Decimals
+  const token0Delta = Math.abs(token0After - token0Before)
+  const token1Delta = Math.abs(token1After - token1Before)
+
+  const formatTokenAmounts = (amount0: number, amount1: number) => {
+    return (
+      `${formatDisplayNumber(amount0, { significantDigits: 4 })} ${token0Symbol}  + ` +
+      `${formatDisplayNumber(amount1, { significantDigits: 4 })} ${token1Symbol}`
+    )
+  }
+
+  const tokenId = getTokenId(positionId)
+
   const onClick = () => {
     const positionUrl = APP_PATHS.EARN_POSITION_DETAIL.replace(':positionId', positionId)
-      .replace(':chainId', rawChain)
+      .replace(':chainId', String(chainId))
       .replace(':exchange', exchange)
     navigate(positionUrl)
     onRead(announcement, statusMessage)
@@ -68,16 +135,14 @@ function InboxItemEarnPosition({
       <InboxItemRow>
         <RowItem>
           <InboxIcon type={templateType} chainId={chainId} />
-          <Title isRead={isRead}>{title}</Title>
+          <Title isRead={isRead}>{statusMessage}</Title>
           {!isRead && <Dot />}
         </RowItem>
-        <RowItem>
-          <StatusBadge color={isInRange ? theme.primary : theme.warning}>{statusMessage}</StatusBadge>
-        </RowItem>
+        <RowItem>{time}</RowItem>
       </InboxItemRow>
 
       <InboxItemRow>
-        <Flex alignItems={'center'} style={{ gap: '4px' }}>
+        <Flex alignItems="center" style={{ gap: '4px' }}>
           <DoubleCurrencyLogoV2
             style={{ marginRight: 10 }}
             logoUrl1={token0LogoURL}
@@ -87,20 +152,69 @@ function InboxItemEarnPosition({
           <PrimaryText style={{ fontSize: 14 }}>
             {token0Symbol}/{token1Symbol}
           </PrimaryText>
+
+          <Flex alignItems="center" p="4px 8px" bg={rgba(theme.white, 0.05)} sx={{ gap: 1, borderRadius: 12 }}>
+            <TokenLogo src={dex.logo} size={14} />
+            <Text fontSize={12} color={theme.subText}>
+              {dex.version}
+            </Text>
+            {tokenId && (
+              <Text fontSize={12} color={theme.subText}>
+                #{tokenId}
+              </Text>
+            )}
+          </Flex>
         </Flex>
-        <PrimaryText>
-          {currentPrice} {token0Symbol}/{token1Symbol}
-        </PrimaryText>
       </InboxItemRow>
 
-      <InboxItemRow>
-        <PrimaryText color={theme.subText}>
-          {minPrice} - {maxPrice} {token0Symbol}/{token1Symbol}
-        </PrimaryText>
-        {time}
-      </InboxItemRow>
+      {[Status.NEW, Status.ADD, Status.PARTIAL_REMOVE, Status.FULL_REMOVE].includes(status) && (
+        <InboxItemRow>
+          <Flex alignItems="center" style={{ gap: '4px' }}>
+            <PrimaryText color={theme.subText}>
+              {status === Status.NEW ? t`Created Balance:` : status === Status.ADD ? t`Added:` : t`Removed:`}
+            </PrimaryText>
+            <PrimaryText>
+              {status === Status.NEW
+                ? formatTokenAmounts(token0After, token1After)
+                : formatTokenAmounts(token0Delta, token1Delta)}
+            </PrimaryText>
+          </Flex>
+        </InboxItemRow>
+      )}
+
+      {[Status.IN_RANGE, Status.OUT_RANGE].includes(status) && (
+        <InboxItemRow>
+          <PrimaryText>
+            {currentPrice} {token0Symbol}/{token1Symbol}
+          </PrimaryText>
+          <PrimaryText color={theme.subText}>
+            {minPrice} - {maxPrice}
+          </PrimaryText>
+        </InboxItemRow>
+      )}
     </InboxItemWrapper>
   )
 }
 
 export default InboxItemEarnPosition
+
+const normalizePosition = (position?: PoolPositionAnnouncement | PoolPositionLiquidityAnnouncement) => {
+  if (position && 'notificationType' in position) {
+    const numberRegex = /^-?\d+(\.\d+)?$/
+
+    const normalizedPosition = Object.fromEntries(
+      Object.entries(position).map(([key, value]) => {
+        if (typeof value === 'string' && numberRegex.test(value)) {
+          return [key, Number(value)]
+        }
+        return [key, value]
+      }),
+    ) as PoolPositionLiquidityAnnouncement<number>
+
+    return {
+      ...normalizedPosition,
+      exchange: position.exchange || position.kyberswapUrl?.match(/[^/]+$/)?.[0] || '',
+    }
+  }
+  return position as PoolPositionAnnouncement
+}
