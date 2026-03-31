21db01b0cd4741df240cb01616a9799a2f791dea
Khac Kien <ii.khackien@gmail.com>
2026-03-31 18:00:42 +0700

    Remove Check Skeleton

--- apps/kyberswap-interface/src/pages/Earns/PoolDetail/context.tsx
+++ apps/kyberswap-interface/src/pages/Earns/PoolDetail/context.tsx
@@ -1,24 +1,14 @@
 import { ChainId } from '@kyberswap/ks-sdk-core'
-import { ReactNode, createContext, useContext, useMemo, useState } from 'react'
+import { ReactNode, createContext, useContext, useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { Box } from 'rebass'
 import { PoolDetail, PoolDetailToken, usePoolDetailQuery, usePoolsExplorerQuery } from 'services/zapEarn'
-import styled from 'styled-components'
 
-import { ButtonOutlined } from 'components/Button'
 import { NETWORKS_INFO } from 'constants/networks'
 import { NetworkInfo } from 'constants/networks/type'
 import PoolDetailPageSkeleton from 'pages/Earns/PoolDetail/components/PoolDetailPageSkeleton'
 import { NoteCard, PoolDetailWrapper } from 'pages/Earns/PoolDetail/styled'
 import { EARN_DEXES, EarnDexInfo, Exchange } from 'pages/Earns/constants'
 import { EarnPool } from 'pages/Earns/types'
-
-const TestSkeletonButton = styled(ButtonOutlined)`
-  position: fixed;
-  width: fit-content;
-  top: 80px;
-  right: 40px;
-`
 
 interface PoolDetailContextValue {
   pool: PoolDetail
@@ -109,16 +99,6 @@ export const PoolDetailProvider = ({ children }: { children: ReactNode }) => {
   const isPoolLoading =
     Boolean(poolChainId && poolAddress) && (isPoolDetailLoading || (Boolean(exchange) && isExplorerLoading))
 
-  const [checkSkeleton, setCheckSkeleton] = useState(false)
-
-  if (checkSkeleton)
-    return (
-      <Box width="100%">
-        <TestSkeletonButton onClick={() => setCheckSkeleton(false)}>Check Skeleton</TestSkeletonButton>
-        <PoolDetailPageSkeleton />
-      </Box>
-    )
-
   if (!poolChainId || !poolAddress) {
     return (
       <PoolDetailWrapper>
@@ -157,12 +137,7 @@ export const PoolDetailProvider = ({ children }: { children: ReactNode }) => {
     feeTier,
   }
 
-  return (
-    <PoolDetailContext.Provider value={value}>
-      <TestSkeletonButton onClick={() => setCheckSkeleton(true)}>Check Skeleton</TestSkeletonButton>
-      {children}
-    </PoolDetailContext.Provider>
-  )
+  return <PoolDetailContext.Provider value={value}>{children}</PoolDetailContext.Provider>
 }
 
 export const usePoolDetailContext = () => {
