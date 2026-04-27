3158c6890de175580c4891689b713abada30345a
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2026-04-28 00:04:34 +0700

    chore(kyberswap-interface): drop storybook tooling and config
    - remove storybook + build-storybook scripts from package.json
    - remove @storybook/{addon-essentials,addon-interactions,addon-links,
      addon-mdx-gfm,addon-onboarding,react,react-vite}, storybook, and
      eslint-plugin-storybook devDependencies
    - remove plugin:storybook/recommended from .eslintrc.js extends
    - remove storybook-static from .gitignore
    - remove storybook command line from README.md
    - remove src/**/*.stories.{ts,tsx} from knip ignore patterns

--- apps/kyberswap-interface/.eslintrc.js
+++ apps/kyberswap-interface/.eslintrc.js
@@ -39,7 +39,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'plugin:storybook/recommended',
   ],
   plugins: ['better-styled-components', 'unused-imports', 'jsx-a11y', 'lingui'],
   rules: {
