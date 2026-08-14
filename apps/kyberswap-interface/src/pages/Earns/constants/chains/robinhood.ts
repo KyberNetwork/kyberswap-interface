6fd96c2c9f1fc18cb3bedc346a0816f7dde143b2
Nguyen Van Viet <vietnv1304@gmail.com>
2026-08-14 21:46:50 +0700

    fix: claim reward robinhood (#3339)

--- apps/kyberswap-interface/src/pages/Earns/constants/chains/robinhood.ts
+++ apps/kyberswap-interface/src/pages/Earns/constants/chains/robinhood.ts
@@ -3,7 +3,7 @@ import { ETHER_ADDRESS } from 'constants/index'
 
 export default {
   nativeAddress: ETHER_ADDRESS.toLowerCase(),
-  farmingSupported: false,
+  farmingSupported: true,
   univ4StateViewContract: '0xf3334192d15450cdd385c8b70e03f9a6bd9e673b',
   smartExitSupported: false,
   logo: robinhoodIcon,
