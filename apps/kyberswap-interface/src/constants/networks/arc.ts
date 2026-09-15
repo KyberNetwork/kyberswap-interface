c357dfd5a9ca1e73408d3b47acc4493c78c41eae
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2026-09-15 18:13:53 +0700

    fix: rpc

--- apps/kyberswap-interface/src/constants/networks/arc.ts
+++ apps/kyberswap-interface/src/constants/networks/arc.ts
@@ -32,7 +32,7 @@ const arcInfo: NetworkInfo = {
       payNativeOnSwap: true,
     },
   },
-  defaultRpcUrl: 'https://lb.drpc.live/arc/AgtWW5_pUEE3tn6WXOEX6rIkFRHcBaUR8bS2ehXRfUMv',
+  defaultRpcUrl: 'https://rpc.arc-scan.org',
   multicall: '0xcA11bde05977b3631167028862bE2a173976CA11',
   classic: {
     defaultSubgraph: '',
